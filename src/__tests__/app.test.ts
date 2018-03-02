9b584f467aab19dab1292dd82a4e3bb0c80af043
Luca Lusso <luca.lusso@wellnet.it>
2018-03-02 16:17:00 +0100

    [#155244935] Fix typo. Align package versions. Clear env variable after test.

--- src/__tests__/app.test.ts
+++ src/__tests__/app.test.ts
@@ -5,18 +5,26 @@ jest.mock("../services/redisSessionStorage");
 jest.mock("../services/apiClientFactory");
 
 describe("Test the root path", () => {
-  test("It should response 200 if forwarded from an HTTPS connection", () => {
-    process.env.NODE_ENV = "production";
+  // tslint:disable:no-let
+  let originalNodeEnv: string;
 
+  beforeEach(() => {
+    originalNodeEnv = process.env.NODE_ENV || "development";
+    process.env.NODE_ENV = "production";
+  });
+
+  afterEach(() => {
+    process.env.NODE_ENV = originalNodeEnv;
+  });
+
+  test("It should respond 200 if forwarded from an HTTPS connection", () => {
     return request(app)
       .get("/")
       .set("X-Forwarded-Proto", "https")
       .expect(200);
   });
 
-  test("It should response 301 if forwarded from an HTTP connection", () => {
-    process.env.NODE_ENV = "production";
-
+  test("It should respond 301 if forwarded from an HTTP connection", () => {
     return request(app)
       .get("/")
       .expect(301)
