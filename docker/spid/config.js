88b95858c673f7732ddfca26ca4777785cae5596
Lusso Luca <lussoluca@gmail.com>
2018-09-12 07:22:44 +0200

    [#158907283] Remove latest references to the old spid-testenv-identityserver

--- docker/spid/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-
-  wso2_url  : "https://spid-testenv-identityserver:9443",
-  wso2_user : 'admin',
-  wso2_pass : 'admin',
-  import_path	: 'import/spid-users.json'
-};
-
-module.exports = config;
