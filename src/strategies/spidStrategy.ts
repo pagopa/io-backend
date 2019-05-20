1f564263af21b4875b1cc08ad1e5c55aa57caf1a
Daniele Manni <dan.manni@gmail.com>
2019-05-20 18:13:13 +0200

    [#164984241] use Array instead NonEmptyArray for idp options

--- src/strategies/spidStrategy.ts
+++ src/strategies/spidStrategy.ts
@@ -2,13 +2,11 @@
  * Builds and configure a Passport strategy to authenticate the proxy to the
  * different SPID IDPs.
  */
-import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
-import { NonEmptyString } from "italia-ts-commons/lib/strings";
 import * as SpidStrategy from "spid-passport";
-import { IDPEntityDescriptor } from "../types/IDPEntityDescriptor";
 import { SpidUser } from "../types/user";
 import {
   fetchIdpMetadata,
+  IDPOption,
   mapIpdMetadata,
   parseIdpMetadata
 } from "../utils/idpLoader";
@@ -32,7 +30,7 @@ const IDP_IDS: { [key: string]: string | undefined } = {
  */
 export async function loadFromRemote(
   idpMetadataUrl: string
-): Promise<{ [key: string]: IDPEntityDescriptor | undefined }> {
+): Promise<{ [key: string]: IDPOption | undefined }> {
   log.info("Fetching SPID metadata from [%s]...", idpMetadataUrl);
   const idpMetadataXML = await fetchIdpMetadata(idpMetadataUrl);
   log.info("Parsing SPID metadata...");
@@ -57,19 +55,17 @@ const spidStrategy = async (
 ) => {
   const idpsMetadataOption = await loadFromRemote(IDPMetadataUrl);
 
-  const testenv2Cert = new NonEmptyArray<NonEmptyString>(
-    "MIIC7TCCAdWgAwIBAgIJAMbxPOoBth1LMA0GCSqGSIb3DQEBCwUAMA0xCzAJBgNVBAYTAklUMB4XDTE4MDkwNDE0MDAxM1oXDTE4MTAwNDE0MDAxM1owDTELMAkGA1UEBhMCSVQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJrW3y8Zd2jESPXGMRY04cHC4Qfo3302HEY1C6x1aDfW7aR/tXzNplfdw8ZtZugSSmHZBxVrR8aA08dUVbbtUw5qD0uAWKIeREqGfhM+J1STAMSI2/ZxA6t2fLmv8l1eRd1QGeRDm7yF9EEKGY9iUZD3LJf2mWdVBAzzYlG23M769k+9JuGZxuviNWMjojgYRiQFgzypUJJQz+Ihh3q7LMjjiQiiULVb9vnJg7UdU9Wf3xGRkxk6uiGP9SzWigSObUekYYQ4ZAI/spILywgDxVMMtv/eVniUFKLABtljn5cE9zltECahPbm7wIuMJpDDu5GYHGdYO0j+K7fhjvF2mzAgMBAAGjUDBOMB0GA1UdDgQWBBQEVmzA/L1/fd70ok+6xtDRF8A3HjAfBgNVHSMEGDAWgBQEVmzA/L1/fd70ok+6xtDRF8A3HjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCRMo4M4PqS0iLTTRWfikMF4hYMapcpmuna6p8aee7CwTjS5y7y18RLvKTi9l8OI0dVkgokH8fq8/o13vMw4feGxro1hMeUilRtH52funrWC+FgPrqk3o/8cZOnq+CqnFFDfILLiEb/PVJMddvTXgv2f9O6u17f8GmMLzde1yvYDa1fG/Pi0fG2F0yw/CmtP8OTLSvxjPtJ+ZckGzZa9GotwHsoVJ+Od21OU2lOeCnOjJOAbewHgqwkCB4O4AT5RM4ThAQtoU8QibjD1XDk/ZbEHdKcofnziDyl0V8gglP2SxpzDaPX0hm4wgHk9BOtSikb72tfOw+pNfeSrZEr6ItQ" as NonEmptyString,
-    []
-  );
   const options: {
-    idp: { [key: string]: IDPEntityDescriptor | undefined };
+    idp: { [key: string]: IDPOption | undefined };
     // tslint:disable-next-line: no-any
     sp: any;
   } = {
     idp: {
       ...idpsMetadataOption,
       xx_testenv2: {
-        cert: testenv2Cert,
+        cert: [
+          "MIIC7TCCAdWgAwIBAgIJAMbxPOoBth1LMA0GCSqGSIb3DQEBCwUAMA0xCzAJBgNVBAYTAklUMB4XDTE4MDkwNDE0MDAxM1oXDTE4MTAwNDE0MDAxM1owDTELMAkGA1UEBhMCSVQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDJrW3y8Zd2jESPXGMRY04cHC4Qfo3302HEY1C6x1aDfW7aR/tXzNplfdw8ZtZugSSmHZBxVrR8aA08dUVbbtUw5qD0uAWKIeREqGfhM+J1STAMSI2/ZxA6t2fLmv8l1eRd1QGeRDm7yF9EEKGY9iUZD3LJf2mWdVBAzzYlG23M769k+9JuGZxuviNWMjojgYRiQFgzypUJJQz+Ihh3q7LMjjiQiiULVb9vnJg7UdU9Wf3xGRkxk6uiGP9SzWigSObUekYYQ4ZAI/spILywgDxVMMtv/eVniUFKLABtljn5cE9zltECahPbm7wIuMJpDDu5GYHGdYO0j+K7fhjvF2mzAgMBAAGjUDBOMB0GA1UdDgQWBBQEVmzA/L1/fd70ok+6xtDRF8A3HjAfBgNVHSMEGDAWgBQEVmzA/L1/fd70ok+6xtDRF8A3HjAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBCwUAA4IBAQCRMo4M4PqS0iLTTRWfikMF4hYMapcpmuna6p8aee7CwTjS5y7y18RLvKTi9l8OI0dVkgokH8fq8/o13vMw4feGxro1hMeUilRtH52funrWC+FgPrqk3o/8cZOnq+CqnFFDfILLiEb/PVJMddvTXgv2f9O6u17f8GmMLzde1yvYDa1fG/Pi0fG2F0yw/CmtP8OTLSvxjPtJ+ZckGzZa9GotwHsoVJ+Od21OU2lOeCnOjJOAbewHgqwkCB4O4AT5RM4ThAQtoU8QibjD1XDk/ZbEHdKcofnziDyl0V8gglP2SxpzDaPX0hm4wgHk9BOtSikb72tfOw+pNfeSrZEr6ItQ"
+        ],
         entityID: "xx_testenv2",
         entryPoint: spidTestEnvUrl + "/sso",
         logoutUrl: spidTestEnvUrl + "/slo"
