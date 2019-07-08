82d13ac8951d64393a7b43988dffaa188fcb5944
Daniele Manni <dan.manni@gmail.com>
2019-07-08 10:14:20 +0200

    remove async on express handler

--- src/utils/express.ts
+++ src/utils/express.ts
@@ -13,17 +13,19 @@ export function toExpressHandler<T, P>(
   handler: (req: express.Request) => Promise<IResponse<T>>,
   object?: P
 ): (req: express.Request, res: express.Response) => void {
-  return async (req, res) => {
-    try {
-      const response = await handler.call(object, req);
-      // tslint:disable-next-line:no-object-mutation
-      res.locals.detail = response.detail;
-      response.apply(res);
-    } catch (e) {
-      const response = ResponseErrorInternal(e);
-      // tslint:disable-next-line:no-object-mutation
-      res.locals.detail = response.detail;
-      response.apply(res);
-    }
+  return (req, res) => {
+    handler
+      .call(object, req)
+      .then(response => {
+        // tslint:disable-next-line:no-object-mutation
+        res.locals.detail = response.detail;
+        response.apply(res);
+      })
+      .catch(e => {
+        const response = ResponseErrorInternal(e);
+        // tslint:disable-next-line:no-object-mutation
+        res.locals.detail = response.detail;
+        response.apply(res);
+      });
   };
 }
