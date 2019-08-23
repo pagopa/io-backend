becc7873fc6ec85ba2531a4c219d31ea7bbcf1e0
Daniele Manni <dan.manni@gmail.com>
2019-08-23 12:16:59 +0200

    [#166785497] add doc for errorFormatter util functions

--- src/utils/errorsFormatter.ts
+++ src/utils/errorsFormatter.ts
@@ -1,7 +1,18 @@
+/**
+ * Stringify the Error
+ * @param error
+ * @returns A formatted string of the error.
+ */
 function getMessage(error: Error): string {
   return `value [${JSON.stringify(error)}]`;
 }
 
+/**
+ * Merge into one single Error several errors provided in input and add a context description
+ * @param errors
+ * @param context
+ * @returns A single Error instance with a formatted message.
+ */
 export function multipleErrorsFormatter(
   errors: ReadonlyArray<Error>,
   context: string
