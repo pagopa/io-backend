b71f91152eeebc9c7c25f8108b816f7aa9f15f22
Davide Chiabrero <davide.chiabrero@it.ibm.com>
2019-08-29 08:33:27 +0200

    [#166152893] Gracefully shutdown on SIGTERM and SIGINT  (#431)

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import { NodeEnvironmentEnum } from "italia-ts-commons/lib/environment";
 import { CIDR } from "italia-ts-commons/lib/strings";
 import { newApp } from "./app";
 import container, { SAML_CERT, SAML_KEY } from "./container";
+import { initHttpGracefulShutdown } from "./utils/gracefulShutdown";
 import { log } from "./utils/logger";
 
 const port = container.resolve<number>("serverPort");
@@ -25,8 +26,20 @@ const authenticationBasePath = container.resolve<string>(
 const APIBasePath = container.resolve<string>("APIBasePath");
 const PagoPABasePath = container.resolve<string>("PagoPABasePath");
 
+// Set default for graceful-shutdown
+const DEFAULT_SHUTDOWN_SIGNALS = "SIGINT SIGTERM";
+const DEFAULT_SHUTDOWN_TIMEOUT_MILLIS = 30000;
+
+const shutdownSignals: string =
+  process.env.SHUTDOWN_SIGNALS || DEFAULT_SHUTDOWN_SIGNALS;
+
+const shutdownTimeout: number = process.env.DEFAULT_SHUTDOWN_TIMEOUT_MILLIS
+  ? parseInt(process.env.DEFAULT_SHUTDOWN_TIMEOUT_MILLIS, 10)
+  : DEFAULT_SHUTDOWN_TIMEOUT_MILLIS;
+
 // tslint:disable-next-line: no-let
 let server: http.Server | https.Server;
+
 newApp(
   env,
   allowNotifyIPSourceRange,
@@ -50,9 +63,19 @@ newApp(
         log.info("Listening on port %d", port);
       });
     }
+
     server.on("close", () => {
       app.emit("server:stop");
       log.info("HTTP server close.");
+    });
+
+    initHttpGracefulShutdown(server, app, {
+      development: env === NodeEnvironmentEnum.DEVELOPMENT,
+      finally: () => {
+        log.info("Server graceful shutdown complete.");
+      },
+      signals: shutdownSignals,
+      timeout: shutdownTimeout
     });
   })
   .catch(err => {
