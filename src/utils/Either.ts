b02784ba522a226bc88c760aad0d1c0a21db7ff4
Daniele Manni <dan.manni@gmail.com>
2019-10-15 10:51:39 +0200

    [#168929351] add comments and docs for backported Either functions

--- src/utils/Either.ts
+++ src/utils/Either.ts
@@ -1,6 +1,51 @@
+/**
+ * IMPORTANT: This is a partial backport of the `Either` implementation from
+ * the version v2.0.0 of `fp-ts` library.
+ *
+ *
+ * @file Represents a value of one of two possible types (a disjoint union).
+ *
+ *
+ * An instance of `Either` is either an instance of `Left` or `Right`.
+ *
+ * A common use of `Either` is as an alternative to `Option` for dealing with possible missing values. In this usage,
+ * `None` is replaced with a `Left` which can contain useful information. `Right` takes the place of `Some`. Convention
+ * dictates that `Left` is used for failure and `Right` is used for success.
+ *
+ * For example, you could use `Either<string, number>` to detect whether a received input is a `string` or a `number`.
+ *
+ * ```ts
+ * const parse = (errorMessage: string) => (input: string): Either<string, number> => {
+ *   const n = parseInt(input, 10)
+ *   return isNaN(n) ? left(errorMessage) : right(n)
+ * }
+ * ```
+ *
+ * `Either` is right-biased, which means that `Right` is assumed to be the default case to operate on. If it is `Left`,
+ * operations like `map`, `chain`, ... return the `Left` value unchanged:
+ *
+ * ```ts
+ * import { either } from 'fp-ts/lib/Either'
+ *
+ * either.map(right(12), double) // right(24)
+ * either.map(left(23), double)  // left(23)
+ * ```
+ */
+
 import { Either, left, right } from "fp-ts/lib/Either";
 import { Lazy } from "fp-ts/lib/function";
 
+/**
+ * Converts a JavaScript Object Notation (JSON) string into an object.
+ *
+ * @example
+ * import { parseJSON, toError, right, left } from 'fp-ts/lib/Either'
+ *
+ * assert.deepStrictEqual(parseJSON('{"a":1}', toError), right({ a: 1 }))
+ * assert.deepStrictEqual(parseJSON('{"a":}', toError), left(new SyntaxError('Unexpected token } in JSON at position 5')))
+ *
+ * @since 2.0.0
+ */
 export function parseJSON<E>(
   s: string,
   onError: (reason: unknown) => E
@@ -8,6 +53,19 @@ export function parseJSON<E>(
   return tryCatch(() => JSON.parse(s), onError);
 }
 
+/**
+ * Converts a JavaScript value to a JavaScript Object Notation (JSON) string.
+ *
+ * @example
+ * import { stringifyJSON, toError, right, left } from 'fp-ts/lib/Either'
+ *
+ * assert.deepStrictEqual(stringifyJSON({ a: 1 }, toError), right('{"a":1}'))
+ * const circular: any = { ref: null }
+ * circular.ref = circular
+ * assert.deepStrictEqual(stringifyJSON(circular, toError), left(new TypeError('Converting circular structure to JSON')))
+ *
+ * @since 2.0.0
+ */
 export function stringifyJSON<E>(
   u: unknown,
   onError: (reason: unknown) => E
@@ -15,6 +73,29 @@ export function stringifyJSON<E>(
   return tryCatch(() => JSON.stringify(u), onError);
 }
 
+/**
+ * Constructs a new `Either` from a function that might throw
+ *
+ * @example
+ * import { Either, left, right, tryCatch } from 'fp-ts/lib/Either'
+ *
+ * const unsafeHead = <A>(as: Array<A>): A => {
+ *   if (as.length > 0) {
+ *     return as[0]
+ *   } else {
+ *     throw new Error('empty array')
+ *   }
+ * }
+ *
+ * const head = <A>(as: Array<A>): Either<Error, A> => {
+ *   return tryCatch(() => unsafeHead(as), e => (e instanceof Error ? e : new Error('unknown error')))
+ * }
+ *
+ * assert.deepStrictEqual(head([]), left(new Error('empty array')))
+ * assert.deepStrictEqual(head([1, 2, 3]), right(1))
+ *
+ * @since 2.0.0
+ */
 export function tryCatch<E, A>(
   f: Lazy<A>,
   onError: (e: unknown) => E
