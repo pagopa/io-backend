4072eca674794da4d6c201ce465964a540e9dae4
danilo.spinelli <danilo.spinelli@pagopa.it>
2020-03-03 12:02:54 +0100

    adds tests

--- src/utils/middleware/rateLimiter.ts
+++ src/utils/middleware/rateLimiter.ts
@@ -1,41 +1,44 @@
 import { NextFunction, Request, Response } from "express";
-import {
-  RateLimiterRes,
-  RateLimiterStoreAbstract
-} from "rate-limiter-flexible";
+import { RateLimiterStoreAbstract } from "rate-limiter-flexible";
+import * as requestIp from "request-ip";
 
+import { ProblemJson } from "italia-ts-commons/lib/responses";
 import { log } from "../../utils/logger";
 
 export const makeRateLimiterMiddleware = (
   rateLimiter: RateLimiterStoreAbstract
-) => (req: Request, res: Response, next: NextFunction) => {
-  const rateLimiterKey = req.ip;
-  return rateLimiter
-    .consume(rateLimiterKey)
-    .then(_ => {
-      res
-        .set("X-RateLimit-Remaining", _.remainingPoints.toString())
-        .set(
-          "X-RateLimit-Reset",
-          new Date(Date.now() + Number(_.msBeforeNext)).toString()
-        )
-        .set(
-          "X-RateLimit-Limit",
-          (_.remainingPoints + _.consumedPoints).toString()
-        );
-      next();
-    })
-    .catch((rateLimiterRes: RateLimiterRes) => {
-      const retryAfter = Math.ceil(rateLimiterRes.msBeforeNext / 1000);
-      log.debug("rate limiter blocking = %s", JSON.stringify(rateLimiterRes));
-      res
-        .set("Retry-After", retryAfter.toString())
-        .set("X-RateLimit-Remaining", rateLimiterRes.remainingPoints.toString())
-        .set(
-          "X-RateLimit-Reset",
-          new Date(Date.now() + Number(rateLimiterRes.msBeforeNext)).toString()
-        )
-        .status(429)
-        .send("Too Many Requests");
-    });
+) => async (req: Request, res: Response, next: NextFunction) => {
+  const ip = requestIp.getClientIp(req);
+  try {
+    const rl = await rateLimiter.consume(ip);
+    res
+      .set("X-RateLimit-Remaining", rl.remainingPoints.toString())
+      .set(
+        "X-RateLimit-Reset",
+        new Date(Date.now() + Number(rl.msBeforeNext)).toString()
+      )
+      .set(
+        "X-RateLimit-Limit",
+        (Number(rl.remainingPoints) + Number(rl.consumedPoints)).toString()
+      );
+    next();
+  } catch (_) {
+    const retryAfter = Math.ceil(_.msBeforeNext / 1000);
+    const problem: ProblemJson = {
+      detail: "Rate limit reached",
+      status: 429,
+      title: "Too Many requests"
+    };
+    log.warn("Rate limiter is blocking ip (%s)", ip);
+    log.warn("Rate limiter is blocking ip (%s) (%s)", ip, _);
+    res
+      .set("X-RateLimit-Remaining", _.remainingPoints.toString())
+      .set(
+        "X-RateLimit-Reset",
+        new Date(Date.now() + Number(_.msBeforeNext)).toString()
+      )
+      .set("Retry-After", retryAfter.toString())
+      .status(429)
+      .json(problem);
+  }
 };
