21a78134c235e4e3c5927e06fc278a199e293a8b
Federico Feroldi <cloudify@users.noreply.github.com>
2020-04-02 18:04:33 +0200

    [#172124428] Adds option to enable keepalive in fetch client (#627)

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
 /**
  * Defines services and register them to the Service Container.
  */
+
+import * as agentkeepalive from "agentkeepalive";
 import * as dotenv from "dotenv";
 import { parseJSON, toError } from "fp-ts/lib/Either";
 import { fromNullable, isSome } from "fp-ts/lib/Option";
+import nodeFetch from "node-fetch";
+
 import { getNodeEnvironmentFromProcessEnv } from "italia-ts-commons/lib/environment";
 import {
   errorsToReadableMessages,
@@ -222,7 +226,62 @@ export const ALLOW_PAGOPA_IP_SOURCE_RANGE = decodeCIDRs(
 export const API_KEY = getRequiredENVVar("API_KEY");
 export const API_URL = getRequiredENVVar("API_URL");
 
-export const API_CLIENT = new ApiClientFactory(API_KEY, API_URL);
+// whether we want to reuse sockets in the fetch client
+// this is expecially useful to avoid SNAT exhaustion in Azure
+// see https://blog.botframework.com/2018/03/05/fix-snat-exhaustion-node-js-bots/
+const isFetchKeepaliveEnabled = process.env.FETCH_KEEPALIVE_ENABLED === "true";
+
+// Returns a fetch instance backed by a keepalive-enabled HTTP agent
+const getKeepaliveFetch: () => typeof fetch = () => {
+  // custom HTTP agent that will reuse sockets
+  // see https://github.com/node-modules/agentkeepalive#new-agentoptions
+  const keepaliveAgent = new agentkeepalive.default({
+    freeSocketTimeout:
+      process.env.FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT === undefined
+        ? undefined
+        : parseInt(process.env.FETCH_KEEPALIVE_FREE_SOCKET_TIMEOUT, 10),
+    keepAlive: true,
+    keepAliveMsecs:
+      process.env.FETCH_KEEPALIVE_KEEPALIVE_MSECS === undefined
+        ? undefined
+        : parseInt(process.env.FETCH_KEEPALIVE_KEEPALIVE_MSECS, 10),
+    maxFreeSockets:
+      process.env.FETCH_KEEPALIVE_MAX_FREE_SOCKETS === undefined
+        ? undefined
+        : parseInt(process.env.FETCH_KEEPALIVE_MAX_FREE_SOCKETS, 10),
+    maxSockets:
+      process.env.FETCH_KEEPALIVE_MAX_SOCKETS === undefined
+        ? undefined
+        : parseInt(process.env.FETCH_KEEPALIVE_MAX_SOCKETS, 10),
+    socketActiveTTL:
+      process.env.FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL === undefined
+        ? undefined
+        : parseInt(process.env.FETCH_KEEPALIVE_SOCKET_ACTIVE_TTL, 10),
+    timeout:
+      process.env.FETCH_KEEPALIVE_TIMEOUT === undefined
+        ? undefined
+        : parseInt(process.env.FETCH_KEEPALIVE_TIMEOUT, 10)
+  });
+
+  return (input, init) => {
+    const initWithKeepalive = {
+      ...(init === undefined ? {} : init),
+      agent: keepaliveAgent
+    };
+    // need to cast to any since node-fetch has a slightly different type
+    // signature that DOM's fetch
+    // TODO: possibly avoid using DOM's fetch type altoghether?
+    // tslint:disable-next-line: no-any
+    return nodeFetch(input as any, initWithKeepalive as any) as any;
+  };
+};
+
+const apiFetch: typeof fetch = isFetchKeepaliveEnabled
+  ? getKeepaliveFetch()
+  : // tslint:disable-next-line: no-any
+    (nodeFetch as any);
+
+export const API_CLIENT = new ApiClientFactory(API_KEY, API_URL, apiFetch);
 
 // Set default session duration to 30 days
 const DEFAULT_TOKEN_DURATION_IN_SECONDS = 3600 * 24 * 30;
--- src/services/apiClientFactory.ts
+++ src/services/apiClientFactory.ts
@@ -2,16 +2,28 @@
  * This service builds API clients.
  */
 
+import nodeFetch from "node-fetch";
+
 import { APIClient } from "../clients/api";
 import { IApiClientFactoryInterface } from "./IApiClientFactory";
 
+// TODO: this class is actually useless as APIClient is immutable, it can be removed
 export default class ApiClientFactory implements IApiClientFactoryInterface {
-  constructor(public readonly apiKey: string, public readonly apiUrl: string) {}
+  private apiClient!: ReturnType<APIClient>;
+
+  constructor(
+    private readonly apiKey: string,
+    private readonly apiUrl: string,
+    // tslint:disable-next-line: no-any
+    private readonly fetchApi: typeof fetch = nodeFetch as any
+  ) {
+    this.apiClient = APIClient(this.apiUrl, this.apiKey, this.fetchApi);
+  }
 
   /**
    * {@inheritDoc}
    */
   public getClient(): ReturnType<APIClient> {
-    return APIClient(this.apiUrl, this.apiKey);
+    return this.apiClient;
   }
 }
