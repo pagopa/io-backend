ffc9128d5354ea937965ec17db75c0bda21668b3
AleDore <alessio85.dore@gmail.com>
2020-04-24 17:56:52 +0200

    curly braces omitted

--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -1,7 +1,7 @@
 import * as bwipjs from "bwip-js";
 import { sequenceS } from "fp-ts/lib/Apply";
 import { Either } from "fp-ts/lib/Either";
-import { taskEither, tryCatch } from "fp-ts/lib/TaskEither";
+import { taskEither, tryCatch, taskify } from "fp-ts/lib/TaskEither";
 import * as JsBarcode from "jsbarcode";
 import { DOMImplementation, XMLSerializer } from "xmldom";
 
@@ -20,8 +20,7 @@ interface IBwipOptions {
 
 const toBase64Png = (options: IBwipOptions) =>
   tryCatch(
-    () => {
-      return new Promise<string>((resolve, reject) =>
+    () =>  new Promise<string>((resolve, reject) =>
         bwipjs.toBuffer(options, (err, png) => {
           if (err == null) {
             return resolve(png.toString("base64"));
@@ -29,15 +28,14 @@ const toBase64Png = (options: IBwipOptions) =>
             return reject(new Error(`Cannot generate png barcode|${err}`));
           }
         })
-      );
-    },
+      ),
     errs => new Error(`Cannot generate png barcode|${errs}`)
   );
 
 const toBase64Svg = (text: string) =>
   tryCatch(
-    () => {
-      return new Promise<string>(resolve => {
+    () =>
+      new Promise<string>(resolve => {
         const xmlSerializer = new XMLSerializer();
         const document = new DOMImplementation().createDocument(
           "http://www.w3.org/1999/xhtml",
@@ -58,8 +56,7 @@ const toBase64Svg = (text: string) =>
         const svgBuffer = Buffer.from(svgPlainText);
         const svg = svgBuffer.toString("base64");
         return resolve(svg);
-      });
-    },
+      }),
     errs => new Error(`Cannot generate svg barcode|${errs}`)
   );
 
