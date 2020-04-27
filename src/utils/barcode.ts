170afaf74ca8fdd6fe230ff17fbbdf59f8a1a34e
AleDore <alessio85.dore@gmail.com>
2020-04-27 17:35:56 +0200

    refactor with bwipjs

--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -1,9 +1,7 @@
 import * as bwipjs from "bwip-js";
 import { sequenceS } from "fp-ts/lib/Apply";
-import { Either } from "fp-ts/lib/Either";
-import { taskEither, tryCatch } from "fp-ts/lib/TaskEither";
-import * as JsBarcode from "jsbarcode";
-import { DOMImplementation, XMLSerializer } from "xmldom";
+import { tryCatch2v } from "fp-ts/lib/Either";
+import { fromEither, taskEither, taskify } from "fp-ts/lib/TaskEither";
 
 export interface IBarcodeOutput {
   png: string;
@@ -15,70 +13,46 @@ export interface IBarcodeOutput {
 interface IBwipOptions {
   bcid: string;
   text: string;
-  includetext: boolean;
 }
 
-const toBase64Png = (options: IBwipOptions) =>
-  tryCatch(
-    () =>
-      new Promise<string>((resolve, reject) =>
-        bwipjs.toBuffer(options, (err, png) => {
-          if (err == null) {
-            return resolve(png.toString("base64"));
-          } else {
-            return reject(new Error(`Cannot generate png barcode|${err}`));
-          }
-        })
-      ),
-    errs => new Error(`Cannot generate png barcode|${errs}`)
+const toBase64Svg = (options: IBwipOptions) =>
+  tryCatch2v(
+    () => {
+      const drawsvg = require("bwip-js/examples/drawing-svg");
+      // tslint:disable-next-line: no-any
+      const anyBwipJs = bwipjs as any;
+      anyBwipJs.fixupOptions(options);
+      const svg = anyBwipJs.render(
+        options,
+        drawsvg(options, anyBwipJs.FontLib)
+      );
+      return Buffer.from(svg).toString("base64");
+    },
+    errs => new Error(`Cannot generate svg barcode|${errs}`) as Error | string
   );
 
-const toBase64Svg = (text: string) =>
-  tryCatch(
-    () =>
-      new Promise<string>(resolve => {
-        const xmlSerializer = new XMLSerializer();
-        const document = new DOMImplementation().createDocument(
-          "http://www.w3.org/1999/xhtml",
-          "html",
-          null
-        );
-        const svgNode = document.createElementNS(
-          "http://www.w3.org/2000/svg",
-          "svg"
-        );
+const toBase64Png = taskify(bwipjs.toBuffer);
 
-        JsBarcode(svgNode, text, {
-          xmlDocument: document
-          // tslint:disable-next-line: no-any
-        } as any);
-
-        const svgPlainText = xmlSerializer.serializeToString(svgNode);
-        const svgBuffer = Buffer.from(svgPlainText);
-        const svg = svgBuffer.toString("base64");
-        return resolve(svg);
-      }),
-    errs => new Error(`Cannot generate svg barcode|${errs}`)
-  );
-
-export const toBarcode = (
-  text: string
-): Promise<Either<Error, IBarcodeOutput>> => {
+export const toBarcode = (text: string) => {
   const options = {
     bcid: "code128",
-    includetext: true,
     text
   };
 
   return sequenceS(taskEither)({
     png: toBase64Png(options),
-    svg: toBase64Svg(text)
+    svg: fromEither(toBase64Svg(options))
   })
-    .map(
+    .fold<Error | IBarcodeOutput>(
+      errorOrString =>
+        typeof errorOrString === "string"
+          ? new Error(errorOrString)
+          : errorOrString,
       images =>
         ({
-          ...images,
+          png: images.png.toString("base64"),
           pngMimeType: "image/png",
+          svg: images.svg,
           svgMimeType: "image/svg+xml"
         } as IBarcodeOutput)
     )
