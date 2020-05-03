54e3522ec8bbfc165d5005fa7a440440b263c18c
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-05-03 11:52:19 +0200

    small refactor using typings

--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -1,4 +1,5 @@
 import * as bwipjs from "bwip-js";
+import { ToBufferOptions } from "bwip-js";
 import { sequenceS } from "fp-ts/lib/Apply";
 import { left, right, tryCatch2v } from "fp-ts/lib/Either";
 import {
@@ -8,48 +9,51 @@ import {
   taskify
 } from "fp-ts/lib/TaskEither";
 import { BARCODE_ALGORITHM } from "../../src/config";
+import { DrawingSVG } from "./bwipjs-svg";
 
 export interface IBarcodeOutput {
   png: string;
   svg: string;
 }
 
-interface IBwipOptions {
-  bcid: string;
-  text: string;
-}
-
-// tslint:disable-next-line: no-var-requires
-const drawsvg = require("bwip-js/examples/drawing-svg");
-
-const toBufferSvg = (options: IBwipOptions) =>
+/**
+ * Render an SVG from a string representing a barcode
+ */
+const toBufferSvg = (options: ToBufferOptions) =>
   tryCatch2v(
     () => {
-      // tslint:disable-next-line: no-any
-      const anyBwipJs = bwipjs as any;
-      // see wiki for further informations about fixupOptions https://github.com/metafloor/bwip-js/wiki/Methods-Reference#bwipjsfixupoptions
-      anyBwipJs.fixupOptions(options);
-      const svg = anyBwipJs.render(
-        options,
-        drawsvg(options, anyBwipJs.FontLib)
-      );
+      const opts = { ...options };
+      // This method mutate its argument so we make a clone here
+      // @see https://github.com/metafloor/bwip-js/wiki/Methods-Reference#bwipjsfixupoptions
+      bwipjs.fixupOptions(opts);
+
+      const svg = bwipjs.render(opts, DrawingSVG(opts, bwipjs.FontLib));
       return Buffer.from(svg);
     },
     errs => new Error(`Cannot generate svg barcode|${errs}`) as Error | string
   );
 
+/**
+ * Render a PNG from a string representing a barcode
+ */
 const toBufferPng = taskify(bwipjs.toBuffer);
 
-export function toBarcode(text: string): TaskEither<Error, IBarcodeOutput> {
+/**
+ * Render a tuple (base64-encoded SVG, base64-encoded PNG)
+ * from a string representing a barcode.
+ */
+export function toBarcode(
+  text: string,
+  bcid = BARCODE_ALGORITHM
+): TaskEither<Error, IBarcodeOutput> {
   const options = {
-    bcid: BARCODE_ALGORITHM,
+    bcid,
     text
   };
-
   return sequenceS(taskEither)({
     png: toBufferPng(options),
     svg: fromEither(toBufferSvg(options))
-  }).foldTaskEither<Error, IBarcodeOutput>(
+  }).foldTaskEither(
     errorOrString =>
       fromEither(
         left(
@@ -63,7 +67,7 @@ export function toBarcode(text: string): TaskEither<Error, IBarcodeOutput> {
         right({
           png: images.png.toString("base64"),
           svg: images.svg.toString("base64")
-        } as IBarcodeOutput)
+        })
       )
   );
 }
