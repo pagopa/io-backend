a8bf23caa241d3e0df93ef14ccc24785e8827dec
danilo spinelli <danilo.spinelli@pagopa.itt>
2020-12-04 00:17:54 +0100

    [#176003320] Return 500 instead of 401 on system errors

--- src/utils/strategies.ts
+++ src/utils/strategies.ts
@@ -21,7 +21,7 @@ export function fulfill(
   done: StrategyDoneFunction
 ): void {
   errorOrUser.fold(
-    error => done(undefined, false, error.message),
+    error => done(error.message),
     user => done(undefined, user.isNone() ? false : user.value)
   );
 }
