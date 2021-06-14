c1258c46d0f4d17961d69482e74ed7547a9d5c85
gquadrati <greta.quadrati@pagopa.it>
2021-06-14 10:17:38 +0200

    Fix return status codes

--- src/services/__tests__/eucovidcertService.test.ts
+++ src/services/__tests__/eucovidcertService.test.ts
@@ -1,3 +1,4 @@
+import * as e from "express";
 import * as t from "io-ts";
 import EUCovidCertService from "../eucovidcertService";
 import { EUCovidCertAPIClient } from "../../clients/eucovidcert.client";
@@ -5,6 +6,7 @@ import { aMockedUser } from "../../__mocks__/user_mock";
 import { StatusEnum as RevokedStatusEnum } from "../../../generated/eucovidcert-api/RevokedCertificate";
 import { RevokedCertificate } from "../../../generated/eucovidcert/RevokedCertificate";
 import { ProblemJson } from "@pagopa/ts-commons/lib/responses";
+import mockRes from "../../__mocks__/response";
 
 const mockClientGetCertificate = jest.fn();
 
@@ -58,16 +60,16 @@ describe("EUCovidCertService", () => {
   });
 
   it.each`
-    title                                                                | status_code | value                                                                                     | expected_kind                 | expected_detail
-    ${"return IResponseErrorValidation if status is 400"}                | ${400}      | ${null}                                                                                   | ${"IResponseErrorValidation"} | ${"Bad Request: Payload has bad format"}
-    ${"return IResponseErrorInternal if status is 401"}                  | ${401}      | ${null}                                                                                   | ${"IResponseErrorInternal"}   | ${"Internal server error: Underlying API fails with an unexpected 401"}
-    ${"return IResponseErrorNotFound if status is 403"}                  | ${403}      | ${null}                                                                                   | ${"IResponseErrorNotFound"}   | ${"Not Found: Certificate not found"}
-    ${"return IResponseErrorInternal if status is 500"}                  | ${500}      | ${{ title: "An error", detail: "An error detail", type: "An error type" } as ProblemJson} | ${"IResponseErrorInternal"}   | ${"Internal server error: An error (An error type)"}
-    ${"return IResponseErrorInternal: Gateway Timeout if status is 504"} | ${504}      | ${null}                                                                                   | ${"IResponseErrorInternal"}   | ${"Internal server error: Gateway Timeout"}
-    ${"return IResponseErrorInternal if status code is not in spec"}     | ${418}      | ${null}                                                                                   | ${"IResponseErrorInternal"}   | ${"Internal server error: unhandled API response status [418]"}
+    title                                                                | status_code | value                                                                                     | expected_status_code | expected_kind                 | expected_detail
+    ${"return IResponseErrorValidation if status is 400"}                | ${400}      | ${null}                                                                                   | ${400}               | ${"IResponseErrorValidation"} | ${"Bad Request: Payload has bad format"}
+    ${"return IResponseErrorInternal if status is 401"}                  | ${401}      | ${null}                                                                                   | ${500}               | ${"IResponseErrorInternal"}   | ${"Internal server error: Underlying API fails with an unexpected 401"}
+    ${"return IResponseErrorNotFound if status is 403"}                  | ${403}      | ${null}                                                                                   | ${403}               | ${"IResponseErrorNotFound"}   | ${"Not Found: Access data provided are invalid or no Certificate has been emitted for the given Citizen"}
+    ${"return IResponseErrorInternal if status is 500"}                  | ${500}      | ${{ title: "An error", detail: "An error detail", type: "An error type" } as ProblemJson} | ${500}               | ${"IResponseErrorInternal"}   | ${"Internal server error: An error (An error type)"}
+    ${"return IResponseErrorInternal: Gateway Timeout if status is 504"} | ${504}      | ${null}                                                                                   | ${504}               | ${"IResponseErrorInternal"}   | ${"Gateway Timeout: DGC took to long to respond"}
+    ${"return IResponseErrorInternal if status code is not in spec"}     | ${418}      | ${null}                                                                                   | ${500}               | ${"IResponseErrorInternal"}   | ${"Internal server error: unhandled API response status [418]"}
   `(
     "should $title",
-    async ({ status_code, value, expected_kind, expected_detail }) => {
+    async ({ status_code, value, expected_status_code, expected_kind, expected_detail }) => {
       mockClientGetCertificate.mockImplementation(() =>
         t.success({ status: status_code, value })
       );
@@ -77,6 +79,11 @@ describe("EUCovidCertService", () => {
         aMockedUser,
         aMockedAuthCode
       );
+
+      // Check status code
+      const responseMock: e.Response = mockRes();
+      res.apply(responseMock);
+      expect(responseMock.status).toHaveBeenCalledWith(expected_status_code);
 
       expect(res).toMatchObject({
         kind: expected_kind,
