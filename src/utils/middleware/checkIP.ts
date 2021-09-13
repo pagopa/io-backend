ed860361e81b4bc703a74a570a843e4d2922bb52
fabriziopapi <fabrizio.papi@pagopa.it>
2021-09-13 18:00:42 +0200

    update checkIP.ts

--- src/utils/middleware/checkIP.ts
+++ src/utils/middleware/checkIP.ts
@@ -3,10 +3,11 @@
  */
 
 import * as express from "express";
-import { isLeft } from "fp-ts/lib/Either";
-import { readableReport } from "italia-ts-commons/lib/reporters";
-import { CIDR, IPString } from "italia-ts-commons/lib/strings";
+import * as E from "fp-ts/lib/Either";
+import { readableReport } from "@pagopa/ts-commons/lib/reporters";
+import { CIDR, IPString } from "@pagopa/ts-commons/lib/strings";
 import * as rangeCheck from "range_check";
+import { pipe } from "fp-ts/lib/function";
 import { log } from "../logger";
 
 export default function checkIP(
@@ -24,21 +25,24 @@ export default function checkIP(
     // when the boolean flag "trust proxy" is enabled
     // express takes this from the leftmost value
     // contained in the x-forwarded-for header
-    const errorOrIPString = IPString.decode(req.ip).alt(
-      // use x-client-ip instead of x-forwarded-for
-      // for internal calls (same vnet)
-      IPString.decode(req.headers["x-client-ip"])
+    const errorOrIPString = pipe(
+      IPString.decode(req.ip),
+      E.alt(() =>
+        // use x-client-ip instead of x-forwarded-for
+        // for internal calls (same vnet)
+        IPString.decode(req.headers["x-client-ip"])
+      )
     );
 
-    if (isLeft(errorOrIPString)) {
+    if (E.isLeft(errorOrIPString)) {
       log.error(
         `Cannot decode source IP: (req.ip=${req.ip},x-client-ip=${
           req.headers["x-client-ip"]
-        },error=${readableReport(errorOrIPString.value)}.`
+        },error=${readableReport(errorOrIPString.left)}.`
       );
       res.status(400).send("Bad request");
     } else {
-      const IP = errorOrIPString.value;
+      const IP = errorOrIPString.right;
       if (!rangeCheck.inRange(IP, Array.from(range))) {
         log.error(`Blocked source IP ${IP}.`);
         res.status(401).send("Unauthorized");
