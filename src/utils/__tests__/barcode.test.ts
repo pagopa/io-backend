edc7511d2f654c033c4c6b8082986a846f102d7e
fabriziopapi <fabrizio.papi@pagopa.it>
2021-09-13 17:59:31 +0200

    update barcode.ts

--- src/utils/__tests__/barcode.test.ts
+++ src/utils/__tests__/barcode.test.ts
@@ -4,18 +4,20 @@ process.env = {
   BARCODE_ALGORITHM: "code128"
 };
 
-import { isLeft, isRight } from "fp-ts/lib/Either";
+import * as E from "fp-ts/lib/Either";
 import { toBarcode } from "../barcode";
 const aText = "FRLFRC74E04B157I";
 
 describe("Barcode generator", () => {
   it("should return a valid svg and png", async () => {
-    const errorOrBarcodes = await toBarcode(aText).run();
-    expect(isRight(errorOrBarcodes)).toBeTruthy();
-    expect(errorOrBarcodes.value).toMatchSnapshot();
+    const errorOrBarcodes = await toBarcode(aText)();
+    expect(E.isRight(errorOrBarcodes)).toBeTruthy();
+    if (E.isRight(errorOrBarcodes)) {
+      expect(errorOrBarcodes.right).toMatchSnapshot();
+    }
   });
   it("should fail with empty string", async () => {
-    const errorOrBarcodes = await toBarcode("").run();
-    expect(isLeft(errorOrBarcodes)).toBeTruthy();
+    const errorOrBarcodes = await toBarcode("")();
+    expect(E.isLeft(errorOrBarcodes)).toBeTruthy();
   });
 });
--- src/utils/barcode.ts
+++ src/utils/barcode.ts
@@ -1,13 +1,10 @@
 import * as bwipjs from "bwip-js";
 import { ToBufferOptions } from "bwip-js";
-import { sequenceS } from "fp-ts/lib/Apply";
-import { left, right, tryCatch2v } from "fp-ts/lib/Either";
-import {
-  fromEither,
-  taskEither,
-  TaskEither,
-  taskify
-} from "fp-ts/lib/TaskEither";
+import * as AP from "fp-ts/lib/Apply";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import * as TE from "fp-ts/lib/TaskEither";
+import { TaskEither } from "fp-ts/lib/TaskEither";
 import { BARCODE_ALGORITHM } from "../../src/config";
 import { DrawingSVG } from "./bwipjs-svg";
 
@@ -20,7 +17,7 @@ export interface IBarcodeOutput {
  * Render an SVG from a string representing a barcode
  */
 const toBufferSvg = (options: ToBufferOptions) =>
-  tryCatch2v(
+  E.tryCatch(
     () => {
       const opts = { ...options };
       // This method mutate its argument so we make a clone here
@@ -36,7 +33,7 @@ const toBufferSvg = (options: ToBufferOptions) =>
 /**
  * Render a PNG from a string representing a barcode
  */
-const toBufferPng = taskify(bwipjs.toBuffer);
+const toBufferPng = TE.taskify(bwipjs.toBuffer);
 
 /**
  * Render a tuple (base64-encoded SVG, base64-encoded PNG)
@@ -50,24 +47,27 @@ export function toBarcode(
     bcid,
     text
   };
-  return sequenceS(taskEither)({
-    png: toBufferPng(options),
-    svg: fromEither(toBufferSvg(options))
-  }).foldTaskEither(
-    errorOrString =>
-      fromEither(
-        left(
-          typeof errorOrString === "string"
-            ? new Error(errorOrString)
-            : errorOrString
+  return pipe(
+    AP.sequenceS(TE.ApplicativePar)({
+      png: toBufferPng(options),
+      svg: TE.fromEither(toBufferSvg(options))
+    }),
+    TE.fold(
+      errorOrString =>
+        TE.fromEither(
+          E.left(
+            typeof errorOrString === "string"
+              ? new Error(errorOrString)
+              : errorOrString
+          )
+        ),
+      images =>
+        TE.fromEither(
+          E.right({
+            png: images.png.toString("base64"),
+            svg: images.svg.toString("base64")
+          })
         )
-      ),
-    images =>
-      fromEither(
-        right({
-          png: images.png.toString("base64"),
-          svg: images.svg.toString("base64")
-        })
-      )
+    )
   );
 }
