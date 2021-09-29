b9738547dae4f15e8a72d093b53d8700b726e0f6
Michael Disaro <michael.disaro@gmail.com>
2021-09-29 19:09:00 +0200

    fixed package.json and parameters

--- src/types/parameters.ts
+++ src/types/parameters.ts
@@ -1,7 +1,30 @@
 import * as t from "io-ts";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { NonNegativeIntegerFromString } from "@pagopa/ts-commons/lib/numbers";
-import { BooleanFromString } from "@pagopa/ts-commons/lib/booleans";
+
+/**
+ * This codec is defined in ts-commons, but since ts-commons is at io-ts 1.8.x
+ * we cannot import because it generates an error on types.
+ * 
+ * TODO: import from ts-commons when io-backend will be upgraded to new fp-ts and io-ts
+ */
+type BooleanFromString = t.Type<boolean, string, unknown>;
+
+const BooleanFromString: BooleanFromString = new t.Type<
+  boolean,
+  string,
+  unknown
+>(
+  "BooleanFromString",
+  t.boolean.is,
+  (s, c) =>
+    s === "true"
+      ? t.success(true)
+      : s === "false"
+      ? t.success(false)
+      : t.failure(s, c),
+  String
+);
 
 export const GetMessagesParameters = t.partial({
   /* eslint-disable sort-keys */
