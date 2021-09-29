d9ac9937623bb184833cba68c8b98a5d3ebb3d9a
Matteo Boschi <mbmaken@gmail.com>
2021-09-29 11:23:41 +0200

    improve from review

--- src/types/__tests__/parameters.test.ts
+++ src/types/__tests__/parameters.test.ts
@@ -7,18 +7,18 @@ const decodingCases: ReadonlyArray<[
 ]> = [
   // all fields are optional
   [{}, true],
-  // must be a number
-  [{ pageSize: "1" }, false],
+  [{ pageSize: "1" }, true],
   [{ pageSize: NaN }, false],
-  [{ pageSize: -1 }, false],
-  [{ pageSize: 1 }, true],
   // must be a natural integer
-  [{ enrichResultData: true }, true],
-  // must be a boolean
-  [{ enrichResultData: "true" }, false],
+  [{ pageSize: -1 }, false],
+  [{ pageSize: 1.2 }, false],
+  [{ pageSize: 1 }, false],
+  [{ enrichResultData: true }, false],
+  [{ enrichResultData: "true" }, true],
+  [{ enrichResultData: "Falze" }, false],
   [{
-    pageSize: 1,
-    enrichResultData: true,
+    pageSize: "1",
+    enrichResultData: "false",
     maximumId: "id1",
     minimumId: "id0"
   }, true]
--- src/types/parameters.ts
+++ src/types/parameters.ts
@@ -1,12 +1,19 @@
-import { NonNegativeInteger } from "@pagopa/ts-commons/lib/numbers";
+import {
+  NonNegativeInteger,
+  NonNegativeIntegerFromString
+} from "@pagopa/ts-commons/lib/numbers";
 import * as t from "io-ts";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { withDefault } from "@pagopa/ts-commons/lib/types";
+import { BooleanFromString } from "io-ts-types/lib/boolean/BooleanFromString";
 
 export const GetMessagesParameters = t.partial({
   /* eslint-disable sort-keys */
-  pageSize: withDefault(NonNegativeInteger, 100 as NonNegativeInteger),
-  enrichResultData: withDefault(t.boolean, false),
+  pageSize: withDefault(
+    NonNegativeIntegerFromString,
+    100 as NonNegativeInteger
+  ),
+  enrichResultData: withDefault(BooleanFromString, false),
   maximumId: NonEmptyString,
   minimumId: NonEmptyString
   /* eslint-enable sort-keys */
