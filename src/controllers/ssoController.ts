121d0661931ff7bd09243c7c86ce04baab42bd2d
Daniele Manni <dan.manni@gmail.com>
2022-04-07 16:05:49 +0200

    [#IC-372] more functional getUserForFIMS, fix test init

--- src/controllers/ssoController.ts
+++ src/controllers/ssoController.ts
@@ -2,14 +2,23 @@
  * This controller handles requests made from MyPortal.
  */
 
+import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
 import {
   IResponseErrorNotFound,
-  IResponseErrorTooManyRequests
+  IResponseErrorTooManyRequests,
+  ResponseErrorInternal
 } from "@pagopa/ts-commons/lib/responses";
 import { EmailString } from "@pagopa/ts-commons/lib/strings";
 import * as express from "express";
 import { identity } from "fp-ts/lib/function";
 import { fromNullable, fromPredicate } from "fp-ts/lib/Option";
+import {
+  tryCatch,
+  fromLeft,
+  taskEither,
+  fromEither
+} from "fp-ts/lib/TaskEither";
+import { InitializedProfile } from "generated/backend/InitializedProfile";
 import {
   IResponseErrorInternal,
   IResponseErrorValidation,
@@ -68,7 +77,6 @@ export const getUserForBPD = (
     )
   );
 
-// TODO: We need the fn-app client to get the user profile.
 export const getUserForFIMS = (profileService: ProfileService) => (
   req: express.Request
 ): Promise<
@@ -78,26 +86,50 @@ export const getUserForFIMS = (profileService: ProfileService) => (
   | IResponseErrorNotFound
   | IResponseSuccessJson<FIMSUser>
 > =>
-  withUserFromRequest(req, async user => {
-    const userProfile = await profileService.getProfile(user);
-    if (userProfile.kind !== "IResponseSuccessJson") {
-      return userProfile;
-    }
-    return withValidatedOrInternalError(
-      FIMSUser.decode({
-        acr: user.spid_level,
-        auth_time: user.created_at,
-        date_of_birth: user.date_of_birth,
-        email: fromNullable(userProfile.value.is_email_validated)
-          .chain(fromPredicate(identity))
-          .chain<EmailString | undefined>(() =>
-            fromNullable(userProfile.value.email)
+  withUserFromRequest(req, async user =>
+    taskEither
+      .of<
+        | IResponseErrorInternal
+        | IResponseErrorTooManyRequests
+        | IResponseErrorNotFound
+        | IResponseErrorValidation,
+        undefined
+      >(void 0)
+      .chain(() =>
+        tryCatch(
+          () => profileService.getProfile(user),
+          _ => ResponseErrorInternal("Cannot retrieve profile")
+        )
+      )
+      .chain<InitializedProfile>(r =>
+        r.kind === "IResponseSuccessJson" ? taskEither.of(r.value) : fromLeft(r)
+      )
+      .chain(userProfile =>
+        fromEither(
+          FIMSUser.decode({
+            acr: user.spid_level,
+            auth_time: user.created_at,
+            date_of_birth: user.date_of_birth,
+            email: fromNullable(userProfile.is_email_validated)
+              .chain(fromPredicate(identity))
+              .chain<EmailString | undefined>(() =>
+                fromNullable(userProfile.email)
+              )
+              .getOrElse(undefined),
+            family_name: user.family_name,
+            fiscal_code: user.fiscal_code,
+            name: user.name
+          }).mapLeft(_ =>
+            ResponseErrorInternal(errorsToReadableMessages(_).join(" / "))
           )
-          .getOrElse(undefined),
-        family_name: user.family_name,
-        fiscal_code: user.fiscal_code,
-        name: user.name
-      }),
-      _ => ResponseSuccessJson(_)
-    );
-  });
+        )
+      )
+      .fold<
+        | IResponseErrorInternal
+        | IResponseErrorTooManyRequests
+        | IResponseErrorNotFound
+        | IResponseErrorValidation
+        | IResponseSuccessJson<FIMSUser>
+      >(identity, ResponseSuccessJson)
+      .run()
+  );
