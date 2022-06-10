67e545e5588e02690383ef8627016f86e530a4b0
fabrizio.papi <fabrizio.papi@pagopa.it>
2022-06-10 18:15:14 +0200

    fix lint

--- src/server.ts
+++ src/server.ts
@@ -111,8 +111,8 @@ newApp({
   allowNotifyIPSourceRange: ALLOW_NOTIFY_IP_SOURCE_RANGE,
   allowPagoPAIPSourceRange: ALLOW_PAGOPA_IP_SOURCE_RANGE,
   allowSessionHandleIPSourceRange: ALLOW_SESSION_HANDLER_IP_SOURCE_RANGE,
-  appInsightsClient: O.toUndefined(maybeAppInsightsClient),
   allowZendeskIPSourceRange: ALLOW_ZENDESK_IP_SOURCE_RANGE,
+  appInsightsClient: O.toUndefined(maybeAppInsightsClient),
   authenticationBasePath,
   env: ENV
 })
