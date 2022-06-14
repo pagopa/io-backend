2971fe87a0af200881e9374ec9af41b767388f9d
gquadrati <greta.quadrati@pagopa.it>
2022-06-14 09:37:24 +0200

    [#ICC-97] add getThirdPartyServiceClientFactory and tests

---
+++ src/clients/__tests__/third-party-service-client.test.ts
@@ -0,0 +1,51 @@
+import * as E from "fp-ts/lib/Either";
+
+import { ServiceId } from "../../../generated/io-messages-api/ServiceId";
+import { getThirdPartyServiceClientFactory } from "../third-party-service-client";
+import { ThirdPartyConfig } from "../../utils/thirdPartyConfig";
+
+import { aFiscalCode } from "../../__mocks__/user_mock";
+
+const aValidDetailAuthentication = {
+  type: "API_KEY",
+  header_key_name: "aParamName",
+  key: "aKey"
+};
+
+const aServiceId = "aServiceId" as ServiceId;
+const aValidTestAndProdThirdPartyConfig = ThirdPartyConfig.decode({
+  serviceId: aServiceId,
+  schemaKind: "PN",
+  jsonSchema: "aJsonSchema",
+  prodEnvironment: {
+    baseUrl: "aBaseUrl",
+    detailsAuthentication: aValidDetailAuthentication
+  },
+  testEnvironment: {
+    testUsers: [aFiscalCode],
+    baseUrl: "anotherBaseUrl",
+    detailsAuthentication: aValidDetailAuthentication
+  }
+}).getOrElseL(() => {
+  throw Error("Error decoding ThirdPartyConfig");
+});
+
+const aConfigList = [aValidTestAndProdThirdPartyConfig];
+
+describe("getThirdPartyServiceClientFactory", () => {
+  it("should return an Error if serviceId is not present in config list", async () => {
+    const factory = getThirdPartyServiceClientFactory(aConfigList);
+
+    const res = factory("anotherServiceId" as ServiceId);
+
+    expect(E.isLeft(res)).toBeTruthy();
+  });
+
+  it("should return a client if serviceId is present in config list", async () => {
+    const factory = getThirdPartyServiceClientFactory(aConfigList);
+
+    const res = factory("aServiceId" as ServiceId);
+
+    expect(E.isRight(res)).toBeTruthy();
+  });
+});
--- src/clients/third-party-service-client.ts
+++ src/clients/third-party-service-client.ts
@@ -1,11 +1,18 @@
+import * as E from "fp-ts/lib/Either";
+
 import { FiscalCode } from "generated/io-api/FiscalCode";
+import { ServiceId } from "generated/io-api/ServiceId";
 import nodeFetch from "node-fetch";
-import { ThirdPartyConfig } from "src/utils/thirdPartyConfig";
+import {
+  ThirdPartyConfig,
+  ThirdPartyConfigListFromString
+} from "src/utils/thirdPartyConfig";
 import {
   Client,
   createClient
 } from "../../generated/third-party-service/client";
 
+export type ThirdPartyServiceClient = typeof getThirdPartyServiceClient;
 /**
  *
  * @param thirdPartyConfig
@@ -15,7 +22,7 @@ import {
  */
 export const getThirdPartyServiceClient = (
   thirdPartyConfig: ThirdPartyConfig,
-  fetchApi: typeof fetch = (nodeFetch as unknown) as typeof fetch // TODO: customize fetch with timeout
+  fetchApi: typeof fetch = (nodeFetch as unknown) as typeof fetch
 ) => (fiscalCode: FiscalCode): Client<"fiscal_code"> => {
   const config = thirdPartyConfig.testEnvironment?.testUsers.includes(
     fiscalCode
@@ -25,7 +32,10 @@ export const getThirdPartyServiceClient = (
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       thirdPartyConfig.prodEnvironment ?? thirdPartyConfig.testEnvironment!;
 
-  const fetchApiWithAuthentication = fetchApi;
+  const fetchApiWithAuthentication = (
+    input: RequestInfo,
+    init?: RequestInit | undefined
+  ) => fetchApi(input, { ...init, redirect: "manual" });
 
   return createClient<"fiscal_code">({
     basePath: "",
@@ -34,4 +44,23 @@ export const getThirdPartyServiceClient = (
     withDefaults: op => params => op({ ...params, fiscal_code: fiscalCode })
   });
 };
-export type ThirdPartyServiceClient = typeof getThirdPartyServiceClient;
+
+export type ThirdPartyServiceClientFactory = ReturnType<
+  typeof getThirdPartyServiceClientFactory
+>;
+/**
+ * Returns a ThirdParty service client factory
+ * it returns the correct client based on thirdPartyConfigList and service id
+ * @param thirdPartyConfigList
+ * @param fetchApi
+ * @returns
+ */
+export const getThirdPartyServiceClientFactory = (
+  thirdPartyConfigList: ThirdPartyConfigListFromString,
+  fetchApi: typeof fetch = (nodeFetch as unknown) as typeof fetch
+): ((
+  serviceId: ServiceId
+) => E.Either<Error, ReturnType<ThirdPartyServiceClient>>) => serviceId =>
+  E.fromNullable(Error(`Cannot find configuration for service ${serviceId}`))(
+    thirdPartyConfigList.find(c => c.serviceId === serviceId)
+  ).map(config => getThirdPartyServiceClient(config, fetchApi));
