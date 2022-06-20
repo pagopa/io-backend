12f6c77c9dedc795e8d474a4ac53fee3b15e5804
fabrizio.papi <fabrizio.papi@pagopa.it>
2022-06-20 18:24:37 +0200

    add fetch tests

--- src/adapters/__tests__/pnFetch.test.ts
+++ src/adapters/__tests__/pnFetch.test.ts
@@ -1,7 +1,111 @@
+import * as TE from "fp-ts/TaskEither";
 import * as E from "fp-ts/Either";
+import {
+    createClient
+} from "../../../generated/third-party-service/client";
+import { pnFetch } from "../pnFetch";
+import nodeFetch from "node-fetch";
+import { aFiscalCode } from "../../__mocks__/user_mock";
+import * as pnclient from "../../../src/clients/pn-client";
+import { FullReceivedNotification } from "../../../generated/piattaforma-notifiche/FullReceivedNotification";
+import { RecipientTypeEnum } from "../../../generated/piattaforma-notifiche/NotificationRecipient";
+import { NotificationFeePolicyEnum, PhysicalCommunicationTypeEnum } from "../../../generated/piattaforma-notifiche/NewNotificationRequest";
+import { NotificationStatusEnum } from "../../../generated/piattaforma-notifiche/NotificationStatus";
+import {
+    Client as PnClient
+} from "../../../generated/piattaforma-notifiche/client";
+import { NotificationAttachmentDownloadMetadataResponse } from "../../../generated/piattaforma-notifiche/NotificationAttachmentDownloadMetadataResponse";
+import { pipe } from "fp-ts/lib/function";
 
+const aTimelineId = "a-timeline-id";
+const aDate = new Date();
+const aPnUrl = "https://pn-url";
+const aPnKey = "a-pn-key";
+const aPnAttachmentUrl = `https://a.pn.attachment/attachments/an-attachments-id`;
+const aPnNotificationId = "a-notification-id";
+const aDocIdx = "1";
+const aPnNotificationRecipient = { recipientType: RecipientTypeEnum.PF, taxId: "a-tax-id", denomination: "a-denomination" };
+const aPnDocument = { digests: { sha256: "a-digest" }, contentType: "application/pdf", ref: { key: "a-doc-key", versionToken: "1" }, docIdx: aDocIdx };
+const aPnNotificationObject = {
+    paProtocolNumber: "a-protocol-number", subject: "a-subject", recipients: [aPnNotificationRecipient]
+    , documents: [aPnDocument], notificationFeePolicy: NotificationFeePolicyEnum.FLAT_RATE, physicalCommunicationType: PhysicalCommunicationTypeEnum.SIMPLE_REGISTERED_LETTER, iun: "a-iun", sentAt: aDate.toISOString(), notificationStatus: NotificationStatusEnum.ACCEPTED, notificationStatusHistory: [{ activeFrom: aDate.toISOString(), status: NotificationStatusEnum.ACCEPTED, relatedTimelineElements: [aTimelineId] }], timeline: [{ elementId: aTimelineId }]
+};
+const aPnNotification: FullReceivedNotification = pipe(aPnNotificationObject, FullReceivedNotification.decode, E.getOrElseW(() => { throw new Error("a pn notfication is not valid") }));
+const aPnNotificationDocument: NotificationAttachmentDownloadMetadataResponse = { contentLength: 100, contentType: "application/pdf", filename: "a-file-name", sha256: "a-digest", url: aPnAttachmentUrl };
 
+const dummyGetReceivedNotification = jest.fn();
+const dummyGetSentNotificationDocument = jest.fn();
+dummyGetReceivedNotification.mockImplementation(() => TE.of({
+    status: 200,
+    value: aPnNotification,
+    headers: {}
+})());
+dummyGetSentNotificationDocument.mockImplementation(() => TE.of({
+    status: 200,
+    value: aPnNotificationDocument,
+    headers: {}
+})());
+const dummyPnAPIClient = jest.spyOn(pnclient, "PnAPIClient");
+dummyPnAPIClient.mockImplementation(() => ({
+    getReceivedNotification: dummyGetReceivedNotification,
+    getSentNotificationDocument:dummyGetSentNotificationDocument
+} as unknown as PnClient));
 
 describe("pathParamsFromUrl", () => {
+    beforeEach(() => jest.clearAllMocks());
 
+    it("GIVEN a working PN endpoint WHEN a Third-Party get message is called THEN the get is properly orchestrated on PN endpoints", async () => {
+        const aFetch = pnFetch((nodeFetch as any) as typeof fetch, aPnUrl, aPnKey)
+        const client = createClient({ baseUrl: "https://localhost", fetchApi: aFetch });
+        const result = await client.getThirdPartyMessageDetails({ fiscal_code: aFiscalCode, id: aPnNotificationId });
+        expect(E.isRight(result)).toBeTruthy();
+        if (E.isRight(result)) {
+            expect(result.right).toEqual(
+                expect.objectContaining({
+                    status: 200,
+                    value: { details: aPnNotificationObject, attachments: [{ content_type: aPnNotificationDocument.contentType, id: `D${aPnNotification.documents[0].docIdx}`, name: aPnNotificationDocument.filename, url: new URL(aPnNotificationDocument.url??"").pathname }] }
+                }))
+        }
+        expect(dummyGetReceivedNotification).toHaveBeenCalledTimes(1);
+        expect(dummyGetReceivedNotification).toHaveBeenCalledWith({ApiKeyAuth: aPnKey, iun: aPnNotificationId, "x-pagopa-cx-taxid": aFiscalCode});
+        expect(dummyGetSentNotificationDocument).toHaveBeenCalledTimes(1);
+        expect(dummyGetSentNotificationDocument).toHaveBeenCalledWith({ApiKeyAuth: aPnKey, iun: aPnNotificationId, "x-pagopa-cx-taxid": aFiscalCode, docIdx: Number(aDocIdx)});
+    });
+
+    it("GIVEN a not working PN get message endpoint WHEN a Third-Party get message is called THEN the get is properly orchestrated on PN endpoints returning an error", async () => {
+        dummyGetReceivedNotification.mockImplementationOnce(() => TE.of({status: 400, value: {}})());
+
+        const aFetch = pnFetch((nodeFetch as any) as typeof fetch, aPnUrl, aPnKey)
+        const client = createClient({ baseUrl: "https://localhost", fetchApi: aFetch });
+        const result = await client.getThirdPartyMessageDetails({ fiscal_code: aFiscalCode, id: aPnNotificationId });
+        expect(E.isRight(result)).toBeTruthy();
+        if (E.isRight(result)) {
+            expect(result.right).toEqual(
+                expect.objectContaining({
+                    status: 500
+                }))
+        }
+        expect(dummyGetReceivedNotification).toHaveBeenCalledTimes(1);
+        expect(dummyGetReceivedNotification).toHaveBeenCalledWith({ApiKeyAuth: aPnKey, iun: aPnNotificationId, "x-pagopa-cx-taxid": aFiscalCode});
+        expect(dummyGetSentNotificationDocument).toHaveBeenCalledTimes(0);
+    });
+
+    it("GIVEN a not working PN get document endpoint WHEN a Third-Party get message is called THEN the get is properly orchestrated on PN endpoints returning an error", async () => {
+        dummyGetSentNotificationDocument.mockImplementationOnce(() => TE.of({status: 400, value: {}})());
+
+        const aFetch = pnFetch((nodeFetch as any) as typeof fetch, aPnUrl, aPnKey)
+        const client = createClient({ baseUrl: "https://localhost", fetchApi: aFetch });
+        const result = await client.getThirdPartyMessageDetails({ fiscal_code: aFiscalCode, id: aPnNotificationId });
+        expect(E.isRight(result)).toBeTruthy();
+        if (E.isRight(result)) {
+            expect(result.right).toEqual(
+                expect.objectContaining({
+                    status: 500
+                }))
+        }
+        expect(dummyGetReceivedNotification).toHaveBeenCalledTimes(1);
+        expect(dummyGetReceivedNotification).toHaveBeenCalledWith({ApiKeyAuth: aPnKey, iun: aPnNotificationId, "x-pagopa-cx-taxid": aFiscalCode});
+        expect(dummyGetSentNotificationDocument).toHaveBeenCalledTimes(1);
+        expect(dummyGetSentNotificationDocument).toHaveBeenCalledWith({ApiKeyAuth: aPnKey, iun: aPnNotificationId, "x-pagopa-cx-taxid": aFiscalCode, docIdx: Number(aDocIdx)});
+    });
 });
--- src/adapters/pnFetch.ts
+++ src/adapters/pnFetch.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-params */
 import { URL } from "url";
 import { flow, pipe } from "fp-ts/lib/function";
 import * as t from "io-ts";
@@ -5,60 +6,93 @@ import * as E from "fp-ts/Either";
 import * as TE from "fp-ts/TaskEither";
 import * as RA from "fp-ts/ReadonlyArray";
 import * as O from "fp-ts/Option";
-import { pathParamsFromUrl } from "src/types/pathParams";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
-import { errorsToError } from "src/utils/errorsFormatter";
-import { PnAPIClient } from "src/clients/pn-client";
-import { IResponseType } from "@pagopa/ts-commons/lib/requests";
-import { FullReceivedNotification } from "generated/piattaforma-notifiche/FullReceivedNotification";
-import { ThirdPartyAttachment } from "generated/third-party-service/ThirdPartyAttachment";
-import { ThirdPartyMessageDetail } from "generated/third-party-service/ThirdPartyMessageDetail";
-import { NotificationAttachmentDownloadMetadataResponse } from "generated/piattaforma-notifiche/NotificationAttachmentDownloadMetadataResponse";
 import { ProblemJson } from "@pagopa/ts-commons/lib/responses";
+import { Response as NodeResponse } from "node-fetch";
+import { NotificationDocument } from "generated/piattaforma-notifiche/NotificationDocument";
+import { FullReceivedNotification } from "../../generated/piattaforma-notifiche/FullReceivedNotification";
+import { ThirdPartyAttachment } from "../../generated/third-party-service/ThirdPartyAttachment";
+import { ThirdPartyMessageDetail } from "../../generated/third-party-service/ThirdPartyMessageDetail";
+import { NotificationAttachmentDownloadMetadataResponse } from "../../generated/piattaforma-notifiche/NotificationAttachmentDownloadMetadataResponse";
+import { PnAPIClient } from "../clients/pn-client";
+import { errorsToError } from "../utils/errorsFormatter";
+import { pathParamsFromUrl } from "../types/pathParams";
 
-const getUrl = (input: RequestInfo): string =>
-  typeof input === "string" ? input : input.url;
+const getPath = (input: RequestInfo): string =>
+  pipe(typeof input === "string" ? input : input.url, i => new URL(i).pathname);
 
 export const ThirdPartyMessagesUrl = pathParamsFromUrl(
-  RegExp("^/messages/([^/]+)$"),
+  RegExp("^[/]+messages/([^/]+)$"),
   ([id]) => `/messages/${id}`
 );
 
 const WithFiscalCode = t.interface({ fiscal_code: NonEmptyString });
+type WithFiscalCode = t.TypeOf<typeof WithFiscalCode>;
 
-// IResponseType<S extends number, T, H extends string = never>
-// export const Aaa =  new t.Type<ReadonlyArray<string>, string, unknown>(
-//     "Ass",
-//     (u: unknown): u is ReadonlyArray<string> =>
-//       Array.isArray(u) && u.every(value => typeof value === "string"),
-//     (input, context) =>
-//       pipe(
-//         input,
-//         t.string.decode,
-//         E.chain(
-//           E.fromPredicate(
-//             i => decodeTemplate.test(i),
-//             createSingleError(
-//               input,
-//               context,
-//               `input is not a valid ${decodeTemplate}`
-//             )
-//           )
-//         ),
-//         E.map(i => decodeTemplate.exec(i)),
-//         E.map(O.fromNullable),
-//         E.chain(
-//           E.fromOption(
-//             createSingleError(
-//               input,
-//               context,
-//               `Should not be here: input is a valid decodeTemplate but its execution failed!`
-//             )
-//           )
-//         )
-//       ),
-//     encodeTemplate
-//   );
+const retrieveNotificationDetails = (
+  origFetch: typeof fetch,
+  pnUrl: string,
+  pnApiKey: string,
+  headers: WithFiscalCode,
+  [_, iun]: ReadonlyArray<string>
+) =>
+  pipe(
+    () =>
+      PnAPIClient(pnUrl, origFetch).getReceivedNotification({
+        ApiKeyAuth: pnApiKey,
+        iun,
+        "x-pagopa-cx-taxid": headers.fiscal_code
+      }),
+    TE.mapLeft(errorsToError),
+    TE.chain(
+      TE.fromPredicate(
+        r => r.status === 200,
+        r => Error(`Failed to fetch PN ReceivedNotification: ${r.status}`)
+      )
+    ),
+    TE.map(response => response.value as FullReceivedNotification)
+  );
+
+const retrieveAttachmentsMetadata = (
+  origFetch: typeof fetch,
+  pnUrl: string,
+  pnApiKey: string,
+  headers: WithFiscalCode,
+  [_, iun]: ReadonlyArray<string>,
+  { docIdx }: NotificationDocument
+) =>
+  pipe(
+    () =>
+      PnAPIClient(pnUrl, origFetch).getSentNotificationDocument({
+        ApiKeyAuth: pnApiKey,
+        docIdx: Number(docIdx),
+        iun,
+        "x-pagopa-cx-taxid": headers.fiscal_code
+      }),
+    TE.mapLeft(errorsToError),
+    TE.chain(
+      TE.fromPredicate(
+        r => r.status === 200,
+        r => Error(`Failed to fetch PN SentNotificationDocument: ${r.status}`)
+      )
+    ),
+    TE.map(
+      response =>
+        response.value as NotificationAttachmentDownloadMetadataResponse
+    ),
+    TE.chainEitherK(metadata =>
+      pipe(
+        {
+          content_type: metadata.contentType,
+          id: `D${docIdx}`,
+          name: metadata.filename,
+          url: new URL(metadata.url ?? "").pathname
+        },
+        ThirdPartyAttachment.decode,
+        E.mapLeft(errorsToError)
+      )
+    )
+  );
 
 export const redirectMessages = (
   origFetch: typeof fetch,
@@ -81,61 +115,24 @@ export const redirectMessages = (
         TE.fromEither,
         TE.chain(params =>
           pipe(
-            () =>
-              PnAPIClient(pnUrl, origFetch).getReceivedNotification({
-                ApiKeyAuth: pnApiKey,
-                iun: params[0],
-                "x-pagopa-cx-taxid": headers.fiscal_code
-              }),
-            TE.mapLeft(errorsToError),
-            TE.chain(
-              TE.fromPredicate(
-                r => r.status === 200,
-                r =>
-                  Error(`Failed to fetch PN ReceivedNotification: ${r.status}`)
-              )
+            retrieveNotificationDetails(
+              origFetch,
+              pnUrl,
+              pnApiKey,
+              headers,
+              params
             ),
-            TE.map(response => response.value as FullReceivedNotification),
             TE.chain(receivedNotification =>
               pipe(
                 receivedNotification.documents,
                 RA.map(document =>
-                  pipe(
-                    () =>
-                      PnAPIClient(pnUrl, origFetch).getSentNotificationDocument(
-                        {
-                          ApiKeyAuth: pnApiKey,
-                          docIdx: Number(document.docIdx),
-                          iun: params[0],
-                          "x-pagopa-cx-taxid": headers.fiscal_code
-                        }
-                      ),
-                    TE.mapLeft(errorsToError),
-                    TE.chain(
-                      TE.fromPredicate(
-                        r => r.status === 200,
-                        r =>
-                          Error(
-                            `Failed to fetch PN SentNotificationDocument: ${r.status}`
-                          )
-                      )
-                    ),
-                    TE.map(
-                      response =>
-                        response.value as NotificationAttachmentDownloadMetadataResponse
-                    ),
-                    TE.chainEitherK(metadata =>
-                      pipe(
-                        {
-                          content_type: metadata.contentType,
-                          id: `D${document.docIdx}`,
-                          name: metadata.filename,
-                          url: new URL(metadata.url ?? "").pathname
-                        },
-                        ThirdPartyAttachment.decode,
-                        E.mapLeft(errorsToError)
-                      )
-                    )
+                  retrieveAttachmentsMetadata(
+                    origFetch,
+                    pnUrl,
+                    pnApiKey,
+                    headers,
+                    params,
+                    document
                   )
                 ),
                 TE.sequenceArray,
@@ -160,14 +157,17 @@ export const redirectMessages = (
     ),
     TE.map(
       body =>
-        new Response(JSON.stringify(body), { status: 200, statusText: "OK" })
+        (new NodeResponse(JSON.stringify(body), {
+          status: 200,
+          statusText: "OK"
+        }) as unknown) as Response // cast required: the same cast is used in clients code generation
     ),
     TE.mapLeft(
       error =>
-        new Response(JSON.stringify(error), {
+        (new NodeResponse(JSON.stringify(error), {
           status: error.status,
           statusText: error.title
-        })
+        }) as unknown) as Response // cast required: the same cast is used in clients code generation
     ),
     TE.toUnion
   )();
@@ -177,8 +177,8 @@ export const pnFetch = (
   pnUrl: string,
   pnApiKey: string
 ): typeof fetch => (input: RequestInfo, init?: RequestInit) =>
-  pipe(input, getUrl, url =>
-    ThirdPartyMessagesUrl.is(url)
+  pipe(input, getPath, url =>
+    E.isRight(ThirdPartyMessagesUrl.decode(url))
       ? redirectMessages(origFetch, pnUrl, pnApiKey, url, init)
       : () => {
           throw Error(
