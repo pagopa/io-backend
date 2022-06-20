fcb7f0ef315998a3bbf373daccdee8c3af47e9e7
fabrizio.papi <fabrizio.papi@pagopa.it>
2022-06-20 18:34:09 +0200

    fix lint

--- src/adapters/pnFetch.ts
+++ src/adapters/pnFetch.ts
@@ -50,6 +50,7 @@ const retrieveNotificationDetails = (
         r => Error(`Failed to fetch PN ReceivedNotification: ${r.status}`)
       )
     ),
+    // eslint-disable no-unnecessary-type-assertion
     TE.map(response => response.value as FullReceivedNotification)
   );
 
@@ -78,6 +79,7 @@ const retrieveAttachmentsMetadata = (
     ),
     TE.map(
       response =>
+        // eslint-disable no-unnecessary-type-assertion
         response.value as NotificationAttachmentDownloadMetadataResponse
     ),
     TE.chainEitherK(metadata =>
