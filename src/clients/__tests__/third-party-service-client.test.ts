f8762fea321465c288d0f2deca435e431646c9ea
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2022-06-27 09:27:39 +0200

    [#ICC-75]  Choose auth method for Third Party Services calls (#891)
    * [#ICC-75] add third-party-service client getter
    
    * [#ICC-75] add tests

--- src/clients/__tests__/third-party-service-client.test.ts
+++ src/clients/__tests__/third-party-service-client.test.ts
@@ -1,11 +1,14 @@
 import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
 
 import { ServiceId } from "../../../generated/io-messages-api/ServiceId";
 import { getThirdPartyServiceClientFactory } from "../third-party-service-client";
 import { ThirdPartyConfig } from "../../utils/thirdPartyConfig";
 
+import { getThirdPartyServiceClient } from "../third-party-service-client";
+
 import { aFiscalCode } from "../../__mocks__/user_mock";
-import { pipe } from "fp-ts/lib/function";
+import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 
 const aValidDetailAuthentication = {
   type: "API_KEY",
@@ -14,6 +17,7 @@ const aValidDetailAuthentication = {
 };
 
 const aServiceId = "aServiceId" as ServiceId;
+
 const aValidTestAndProdThirdPartyConfig = pipe(
   {
     serviceId: aServiceId,
@@ -52,5 +56,72 @@ describe("getThirdPartyServiceClientFactory", () => {
     const res = factory("aServiceId" as ServiceId);
 
     expect(E.isRight(res)).toBeTruthy();
+  });
+});
+
+const mockNodeFetch = jest.fn(
+  async (_input: RequestInfo | URL, _init?: RequestInit) =>
+    ({
+      ok: true,
+      status: 200,
+      json: async () => {
+        return {};
+      }
+    } as Response)
+);
+
+const aThirdPartyId = "aThirdPartyId";
+
+describe("third-party-service-client", () => {
+  it("should add ApiKey header to Third Party service call with API_KEY configuration when user is a TEST user", async () => {
+    const client = getThirdPartyServiceClient(
+      aValidTestAndProdThirdPartyConfig,
+      mockNodeFetch
+    )(aFiscalCode);
+
+    client.getThirdPartyMessageDetails({
+      id: aThirdPartyId
+    });
+    const expectedConfig = aValidTestAndProdThirdPartyConfig.testEnvironment!;
+
+    expect(mockNodeFetch).toHaveBeenCalledWith(
+      `${expectedConfig.baseUrl}/messages/${aThirdPartyId}`,
+      {
+        headers: {
+          fiscal_code: aFiscalCode,
+          [expectedConfig.detailsAuthentication.header_key_name]:
+            expectedConfig.detailsAuthentication.key
+        },
+        method: "get",
+        redirect: "manual"
+      }
+    );
+  });
+
+  it("should add ApiKey header to Third Party service call with API_KEY configuration when user is a PROD user", async () => {
+    const aProdFiscalCode = "GRBRPP87L04L741X" as FiscalCode;
+
+    const client = getThirdPartyServiceClient(
+      aValidTestAndProdThirdPartyConfig,
+      mockNodeFetch
+    )(aProdFiscalCode);
+
+    client.getThirdPartyMessageDetails({
+      id: aThirdPartyId
+    });
+    const expectedConfig = aValidTestAndProdThirdPartyConfig.prodEnvironment!;
+
+    expect(mockNodeFetch).toHaveBeenCalledWith(
+      `${expectedConfig.baseUrl}/messages/${aThirdPartyId}`,
+      {
+        headers: {
+          fiscal_code: aProdFiscalCode,
+          [expectedConfig.detailsAuthentication.header_key_name]:
+            expectedConfig.detailsAuthentication.key
+        },
+        method: "get",
+        redirect: "manual"
+      }
+    );
   });
 });
