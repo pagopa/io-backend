fd44c228137358c99582d765bb508a8b09106ea6
Daniele Manni <dan.manni@gmail.com>
2022-07-07 14:43:34 +0200

    [#IOCIT-34] Restore modified pn-clients procedure after file renaming

--- src/clients/pn-clients.ts
+++ src/clients/pn-clients.ts
@@ -3,6 +3,10 @@ import {
   Client,
   createClient
 } from "../../generated/piattaforma-notifiche/client";
+import {
+  Client as AddressBookClient,
+  createClient as createAddressBookClient
+} from "../../generated/piattaforma-notifiche-courtesy/client";
 
 export function PnAPIClient(
   baseUrl: string,
@@ -16,3 +20,21 @@ export function PnAPIClient(
 }
 
 export type PnAPIClient = typeof PnAPIClient;
+
+export const PnAddressBookIOClient = (
+  baseUrl: string,
+  apiKey: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  fetchApi: typeof fetch = (nodeFetch as any) as typeof fetch
+): AddressBookClient<"ApiKeyAuth"> =>
+  createAddressBookClient({
+    basePath: "",
+    baseUrl,
+    fetchApi,
+    withDefaults: op => params =>
+      op({
+        ...params,
+        ApiKeyAuth: apiKey
+      })
+  });
+export type PnAddressBookClient = typeof PnAddressBookIOClient;
