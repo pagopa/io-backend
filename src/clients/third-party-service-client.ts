885a9998ae47a8e91bd8e6348a87f408099fecf5
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2022-08-08 10:46:36 +0200

    [ICC-188] Upgrade `@pagopa/openapi-codegen-ts` and remove custom implementation (#927)
    * [#ICC-188] upgrade codegen
    
    * [#ICC-188] move attachment download to standard implementation

--- src/clients/third-party-service-client.ts
+++ src/clients/third-party-service-client.ts
@@ -4,19 +4,6 @@ import { pipe } from "fp-ts/lib/function";
 import nodeFetch from "node-fetch";
 
 import {
-  bufferArrayResponseDecoder,
-  composeResponseDecoders,
-  createFetchRequestForApi,
-  MapResponseType,
-  ReplaceRequestParams,
-  RequestParams,
-  RequestResponseTypes,
-  ResponseDecoder,
-  TypeofApiCall
-} from "@pagopa/ts-commons/lib/requests";
-import { withoutUndefinedValues } from "@pagopa/ts-commons/lib/types";
-
-import {
   ThirdPartyConfig,
   ThirdPartyConfigListFromString,
   ApiKeyAuthenticationConfig,
@@ -29,10 +16,6 @@ import {
   Client,
   createClient
 } from "../../generated/third-party-service/client";
-import {
-  getThirdPartyMessageAttachmentDecoder,
-  GetThirdPartyMessageAttachmentT
-} from "../../generated/third-party-service/requestTypes";
 
 import { pnFetch } from "../adapters/pnFetch";
 
@@ -89,17 +72,6 @@ const withPNFetch = (serviceId: ServiceId, environment: EnvironmentConfig) => (
 
 // ------------------
 
-type GetThirdPartyMessageAttachmentCustomResponseDecoders = ResponseDecoder<
-  RequestResponseTypes<
-    MapResponseType<GetThirdPartyMessageAttachmentT, 200, Buffer>
-  >
->;
-
-type GetThirdPartyMessageAttachmentCustomT = ReplaceRequestParams<
-  MapResponseType<GetThirdPartyMessageAttachmentT, 200, Buffer>,
-  Omit<RequestParams<GetThirdPartyMessageAttachmentT>, "fiscal_code">
->;
-
 /**
  *
  * @param thirdPartyConfig
@@ -110,16 +82,7 @@ type GetThirdPartyMessageAttachmentCustomT = ReplaceRequestParams<
 export const getThirdPartyServiceClient = (
   thirdPartyConfig: ThirdPartyConfig,
   fetchApi: Fetch
-) => (
-  fiscalCode: FiscalCode
-): {
-  readonly getThirdPartyMessageDetails: Client<
-    "fiscal_code"
-  >["getThirdPartyMessageDetails"];
-  readonly getThirdPartyMessageAttachment: TypeofApiCall<
-    GetThirdPartyMessageAttachmentCustomT
-  >;
-} => {
+) => (fiscalCode: FiscalCode): Client<"fiscal_code"> => {
   const environment = thirdPartyConfig.testEnvironment?.testUsers.includes(
     fiscalCode
   )
@@ -135,46 +98,16 @@ export const getThirdPartyServiceClient = (
     withPNFetch(thirdPartyConfig.serviceId, environment)
   );
 
-  const options = {
+  return createClient<"fiscal_code">({
     basePath: "",
     baseUrl: environment.baseUrl,
-    fetchApi: fetchApiWithRedirectAndAuthentication
-  };
-
-  const binaryArrayDecoder: GetThirdPartyMessageAttachmentCustomResponseDecoders = composeResponseDecoders(
-    bufferArrayResponseDecoder(200),
-    getThirdPartyMessageAttachmentDecoder()
-  ) as GetThirdPartyMessageAttachmentCustomResponseDecoders;
-
-  const getThirdPartyMessageAttachmentCustomT: GetThirdPartyMessageAttachmentCustomT = {
-    headers: () => ({
-      fiscal_code: fiscalCode
-    }),
-    method: "get",
-    query: () => withoutUndefinedValues({}),
-    response_decoder: binaryArrayDecoder,
-    url: ({ ["id"]: id, ["attachment_url"]: attachmentUrl }) =>
-      `${options.basePath}/messages/${id}/${attachmentUrl}`
-  };
-
-  const getThirdPartyMessageAttachmentCustom: TypeofApiCall<GetThirdPartyMessageAttachmentCustomT> = createFetchRequestForApi(
-    getThirdPartyMessageAttachmentCustomT,
-    options
-  );
-
-  const baseClient = createClient<"fiscal_code">({
-    ...options,
+    fetchApi: fetchApiWithRedirectAndAuthentication,
     withDefaults: op => params =>
       op({
         ...params,
         fiscal_code: fiscalCode
       })
   });
-
-  return {
-    getThirdPartyMessageAttachment: getThirdPartyMessageAttachmentCustom,
-    getThirdPartyMessageDetails: baseClient.getThirdPartyMessageDetails
-  };
 };
 
 export type ThirdPartyServiceClientFactory = ReturnType<
