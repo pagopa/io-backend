73a965df8519a266a820489351e8ff061bb17065
Greta Quadrati <75862507+gquadrati@users.noreply.github.com>
2023-02-03 12:22:46 +0100

    [#ICC-266]  Update PN client (#974)
    * [#ICC-266] update openapi + adapter
    
    * [#ICC-266] update mock + test
    
    * [#ICC-266] renamed vars

--- src/__mocks__/pn-response.ts
+++ src/__mocks__/pn-response.ts
@@ -1,196 +1,216 @@
 import { pipe } from "fp-ts/lib/function";
 import * as E from "fp-ts/Either";
-import { FullReceivedNotification } from "../../generated/piattaforma-notifiche/FullReceivedNotification";
 import { errorsToReadableMessages } from "@pagopa/ts-commons/lib/reporters";
+import { ThirdPartyMessage as PNThirdParthyMessage } from "../../generated/piattaforma-notifiche/ThirdPartyMessage";
 
-export const notificationDetailResponseExampleAsObject = 
-{
-    "abstract": "Infrazione del codice della strada Art.666",
-    "paProtocolNumber": "202207051147",
-    "subject": "Multa",
-    "recipients": [
-        {
-            "recipientType": "PF",
-            "taxId": "EEEEEE00E00E000A",
-            "denomination": "Matteo Rossi",
-            "physicalAddress": {
-                "at": "",
-                "address": "U. Bassi 7",
-                "addressDetails": "",
-                "zip": "40033",
-                "municipality": "casalecchio di reno",
-                "municipalityDetails": "",
-                "province": "Bologna",
-                "foreignState": "italia"
-            },
-            "payment": {
-                "noticeCode": "302019876543219876",
-                "creditorTaxId": "77777777777",
-                "pagoPaForm": {
-                    "digests": {
-                        "sha256": "wIe6KnvYKKU/zVF8lE/M11VMhK8IggEuKi1GXjiR1kk="
-                    },
-                    "contentType": "application/pdf",
-                    "ref": {
-                        "key": "PN_NOTIFICATION_ATTACHMENTS-0001-URZZ-CCVL-DXJ4-UXOG",
-                        "versionToken": "jkc2PslrgkkvGH2exaOyFRaV70Ns2lQp"
-                    }
-                }
-            }
+export const notificationDetailResponseExampleAsObject = {
+  abstract: "Infrazione del codice della strada Art.666",
+  paProtocolNumber: "202207051147",
+  subject: "Multa",
+  recipients: [
+    {
+      recipientType: "PF",
+      taxId: "EEEEEE00E00E000A",
+      denomination: "Matteo Rossi",
+      physicalAddress: {
+        at: "",
+        address: "U. Bassi 7",
+        addressDetails: "",
+        zip: "40033",
+        municipality: "casalecchio di reno",
+        municipalityDetails: "",
+        province: "Bologna",
+        foreignState: "italia"
+      },
+      payment: {
+        noticeCode: "302019876543219876",
+        creditorTaxId: "77777777777",
+        pagoPaForm: {
+          digests: {
+            sha256: "wIe6KnvYKKU/zVF8lE/M11VMhK8IggEuKi1GXjiR1kk="
+          },
+          contentType: "application/pdf",
+          ref: {
+            key: "PN_NOTIFICATION_ATTACHMENTS-0001-URZZ-CCVL-DXJ4-UXOG",
+            versionToken: "jkc2PslrgkkvGH2exaOyFRaV70Ns2lQp"
+          }
         }
-    ],
-    "documents": [
+      }
+    }
+  ],
+  documents: [
+    {
+      digests: {
+        sha256: "BuIdviesjkElGiz6cAPWl8BK6nWRyjWMEhgHHJzrOHU="
+      },
+      contentType: "application/pdf",
+      ref: {
+        key: "PN_NOTIFICATION_ATTACHMENTS-0001-HO48-AL2O-SCRZ-PNKW",
+        versionToken: "P7cX2vFo2AvNgn_qEhVIeXuf7N0ERi8m"
+      },
+      title: "Verbale",
+      docIdx: "0"
+    }
+  ],
+  notificationFeePolicy: "DELIVERY_MODE",
+  cancelledIun: "",
+  physicalCommunicationType: "SIMPLE_REGISTERED_LETTER",
+  senderDenomination: "Comune di Milano",
+  senderTaxId: "01199250158",
+  group: "",
+  senderPaId: "d0d28367-1695-4c50-a260-6fda526e9aab",
+  iun: "KMZV-LYMY-TDMZ-202207-J-1",
+  sentAt: "2022-07-05T08:52:06.389+00:00",
+  documentsAvailable: true,
+  notificationStatus: "VIEWED",
+  notificationStatusHistory: [
+    {
+      status: "ACCEPTED",
+      activeFrom: "2022-07-05T08:52:06.389+00:00",
+      relatedTimelineElements: [
+        "KMZV-LYMY-TDMZ-202207-J-1_request_accepted",
+        "KMZV-LYMY-TDMZ-202207-J-1_aar_gen_0",
+        "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.PLATFORM(value=PLATFORM)_attempt_0",
+        "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.SPECIAL(value=SPECIAL)_attempt_0",
+        "KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
+        "public_registry_response_KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
+        "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.GENERAL(value=GENERAL)_attempt_0",
+        "KMZV-LYMY-TDMZ-202207-J-1_schedule_analog_workflow_0"
+      ]
+    },
+    {
+      status: "VIEWED",
+      activeFrom: "2022-07-05T15:36:10.230+00:00",
+      relatedTimelineElements: [
+        "KMZV-LYMY-TDMZ-202207-J-1_notification_viewed_0"
+      ]
+    }
+  ],
+  timeline: [
+    {
+      elementId: "KMZV-LYMY-TDMZ-202207-J-1_request_accepted",
+      timestamp: "2022-07-05T08:52:19.280+00:00",
+      legalFactsIds: [
         {
-            "digests": {
-                "sha256": "BuIdviesjkElGiz6cAPWl8BK6nWRyjWMEhgHHJzrOHU="
-            },
-            "contentType": "application/pdf",
-            "ref": {
-                "key": "PN_NOTIFICATION_ATTACHMENTS-0001-HO48-AL2O-SCRZ-PNKW",
-                "versionToken": "P7cX2vFo2AvNgn_qEhVIeXuf7N0ERi8m"
-            },
-            "title": "Verbale",
-            "docIdx": "0"
+          key: "safestorage://PN_LEGAL_FACTS-0002-YWFW-GGP8-QWYO-7232",
+          category: "SENDER_ACK"
         }
-    ],
-    "notificationFeePolicy": "DELIVERY_MODE",
-    "cancelledIun": "",
-    "physicalCommunicationType": "SIMPLE_REGISTERED_LETTER",
-    "senderDenomination": "Comune di Milano",
-    "senderTaxId": "01199250158",
-    "group": "",
-    "senderPaId": "d0d28367-1695-4c50-a260-6fda526e9aab",
-    "iun": "KMZV-LYMY-TDMZ-202207-J-1",
-    "sentAt": "2022-07-05T08:52:06.389+00:00",
-    "documentsAvailable": true,
-    "notificationStatus": "VIEWED",
-    "notificationStatusHistory": [
+      ],
+      category: "REQUEST_ACCEPTED"
+    },
+    {
+      elementId: "KMZV-LYMY-TDMZ-202207-J-1_aar_gen_0",
+      timestamp: "2022-07-05T08:52:20.074+00:00",
+      legalFactsIds: [],
+      category: "AAR_GENERATION",
+      details: {
+        recIndex: 0,
+        numberOfPages: 1,
+        generatedAarUrl: "safestorage://PN_AAR-0002-WE0Q-X1ZY-ZFCI-ZYM7"
+      }
+    },
+    {
+      elementId:
+        "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.PLATFORM(value=PLATFORM)_attempt_0",
+      timestamp: "2022-07-05T08:52:20.284+00:00",
+      legalFactsIds: [],
+      category: "GET_ADDRESS",
+      details: {
+        recIndex: 0,
+        digitalAddressSource: "PLATFORM"
+      }
+    },
+    {
+      elementId:
+        "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.SPECIAL(value=SPECIAL)_attempt_0",
+      timestamp: "2022-07-05T08:52:20.331+00:00",
+      legalFactsIds: [],
+      category: "GET_ADDRESS",
+      details: {
+        recIndex: 0,
+        digitalAddressSource: "SPECIAL"
+      }
+    },
+    {
+      elementId:
+        "KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
+      timestamp: "2022-07-05T08:52:20.375+00:00",
+      legalFactsIds: [],
+      category: "PUBLIC_REGISTRY_CALL",
+      details: {
+        recIndex: 0,
+        deliveryMode: "DIGITAL",
+        contactPhase: "CHOOSE_DELIVERY",
+        sentAttemptMade: 0
+      }
+    },
+    {
+      elementId:
+        "public_registry_response_KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
+      timestamp: "2022-07-05T08:52:20.635+00:00",
+      legalFactsIds: [],
+      category: "PUBLIC_REGISTRY_RESPONSE",
+      details: {
+        recIndex: 0
+      }
+    },
+    {
+      elementId:
+        "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.GENERAL(value=GENERAL)_attempt_0",
+      timestamp: "2022-07-05T08:52:20.762+00:00",
+      legalFactsIds: [],
+      category: "GET_ADDRESS",
+      details: {
+        recIndex: 0,
+        digitalAddressSource: "GENERAL"
+      }
+    },
+    {
+      elementId: "KMZV-LYMY-TDMZ-202207-J-1_schedule_analog_workflow_0",
+      timestamp: "2022-07-05T08:52:20.860+00:00",
+      legalFactsIds: [],
+      category: "SCHEDULE_ANALOG_WORKFLOW",
+      details: {
+        recIndex: 0
+      }
+    },
+    {
+      elementId: "KMZV-LYMY-TDMZ-202207-J-1_notification_viewed_0",
+      timestamp: "2022-07-05T15:36:10.230+00:00",
+      legalFactsIds: [
         {
-            "status": "ACCEPTED",
-            "activeFrom": "2022-07-05T08:52:06.389+00:00",
-            "relatedTimelineElements": [
-                "KMZV-LYMY-TDMZ-202207-J-1_request_accepted",
-                "KMZV-LYMY-TDMZ-202207-J-1_aar_gen_0",
-                "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.PLATFORM(value=PLATFORM)_attempt_0",
-                "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.SPECIAL(value=SPECIAL)_attempt_0",
-                "KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
-                "public_registry_response_KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
-                "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.GENERAL(value=GENERAL)_attempt_0",
-                "KMZV-LYMY-TDMZ-202207-J-1_schedule_analog_workflow_0"
-            ]
-        },
-        {
-            "status": "VIEWED",
-            "activeFrom": "2022-07-05T15:36:10.230+00:00",
-            "relatedTimelineElements": [
-                "KMZV-LYMY-TDMZ-202207-J-1_notification_viewed_0"
-            ]
+          key: "safestorage://PN_LEGAL_FACTS-0002-YMJ5-JPBC-DVF2-BABG",
+          category: "RECIPIENT_ACCESS"
         }
-    ],
-    "timeline": [
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_request_accepted",
-            "timestamp": "2022-07-05T08:52:19.280+00:00",
-            "legalFactsIds": [
-                {
-                    "key": "safestorage://PN_LEGAL_FACTS-0002-YWFW-GGP8-QWYO-7232",
-                    "category": "SENDER_ACK"
-                }
-            ],
-            "category": "REQUEST_ACCEPTED"
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_aar_gen_0",
-            "timestamp": "2022-07-05T08:52:20.074+00:00",
-            "legalFactsIds": [],
-            "category": "AAR_GENERATION",
-            "details": {
-                "recIndex": 0,
-                "numberOfPages": 1,
-                "generatedAarUrl": "safestorage://PN_AAR-0002-WE0Q-X1ZY-ZFCI-ZYM7"
-            }
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.PLATFORM(value=PLATFORM)_attempt_0",
-            "timestamp": "2022-07-05T08:52:20.284+00:00",
-            "legalFactsIds": [],
-            "category": "GET_ADDRESS",
-            "details": {
-                "recIndex": 0,
-                "digitalAddressSource": "PLATFORM"
-            }
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.SPECIAL(value=SPECIAL)_attempt_0",
-            "timestamp": "2022-07-05T08:52:20.331+00:00",
-            "legalFactsIds": [],
-            "category": "GET_ADDRESS",
-            "details": {
-                "recIndex": 0,
-                "digitalAddressSource": "SPECIAL"
-            }
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
-            "timestamp": "2022-07-05T08:52:20.375+00:00",
-            "legalFactsIds": [],
-            "category": "PUBLIC_REGISTRY_CALL",
-            "details": {
-                "recIndex": 0,
-                "deliveryMode": "DIGITAL",
-                "contactPhase": "CHOOSE_DELIVERY",
-                "sentAttemptMade": 0
-            }
-        },
-        {
-            "elementId": "public_registry_response_KMZV-LYMY-TDMZ-202207-J-1_0_DeliveryModeInt.DIGITAL(value=DIGITAL)_ContactPhaseInt.CHOOSE_DELIVERY(value=CHOOSE_DELIVERY)_0_public_registry_call",
-            "timestamp": "2022-07-05T08:52:20.635+00:00",
-            "legalFactsIds": [],
-            "category": "PUBLIC_REGISTRY_RESPONSE",
-            "details": {
-                "recIndex": 0
-            }
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_get_address0_source_DigitalAddressSourceInt.GENERAL(value=GENERAL)_attempt_0",
-            "timestamp": "2022-07-05T08:52:20.762+00:00",
-            "legalFactsIds": [],
-            "category": "GET_ADDRESS",
-            "details": {
-                "recIndex": 0,
-                "digitalAddressSource": "GENERAL"
-            }
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_schedule_analog_workflow_0",
-            "timestamp": "2022-07-05T08:52:20.860+00:00",
-            "legalFactsIds": [],
-            "category": "SCHEDULE_ANALOG_WORKFLOW",
-            "details": {
-                "recIndex": 0
-            }
-        },
-        {
-            "elementId": "KMZV-LYMY-TDMZ-202207-J-1_notification_viewed_0",
-            "timestamp": "2022-07-05T15:36:10.230+00:00",
-            "legalFactsIds": [
-                {
-                    "key": "safestorage://PN_LEGAL_FACTS-0002-YMJ5-JPBC-DVF2-BABG",
-                    "category": "RECIPIENT_ACCESS"
-                }
-            ],
-            "category": "NOTIFICATION_VIEWED",
-            "details": {
-                "recIndex": 0
-            }
-        }
-    ]
+      ],
+      category: "NOTIFICATION_VIEWED",
+      details: {
+        recIndex: 0
+      }
+    }
+  ]
+};
+
+export const notificationResponseExampleAsObject = {
+  attachments: [
+    {
+      id: "PN_NOTIFICATION_ATTACHMENTS-0001-HO48-AL2O-SCRZ-PNKW",
+      content_type: "application/pdf",
+      name: "Verbale",
+      url:
+        "/delivery/notifications/sent/KMZV-LYMY-TDMZ-202207-J-1/attachments/documents/0"
+    }
+  ],
+  details: notificationDetailResponseExampleAsObject
 };
 
 export const notificationDetailResponseExample = pipe(
-    notificationDetailResponseExampleAsObject,
-    FullReceivedNotification.decode,
-    E.getOrElseW(e => {
-    throw new Error("a pn notfication is not valid: "+ errorsToReadableMessages(e).join("|"));
-    })
-  );
+  notificationResponseExampleAsObject,
+  PNThirdParthyMessage.decode,
+  E.getOrElseW(e => {
+    throw new Error(
+      "a pn notfication is not valid: " +
+        errorsToReadableMessages(e, true).join("|")
+    );
+  })
+);
--- src/clients/__tests__/third-party-service-client.test.ts
+++ src/clients/__tests__/third-party-service-client.test.ts
@@ -10,9 +10,9 @@ import { getThirdPartyServiceClient } from "../third-party-service-client";
 import { aFiscalCode } from "../../__mocks__/user_mock";
 import { FiscalCode } from "@pagopa/ts-commons/lib/strings";
 import {
-  aPnNotification,
-  aPnNotificationObject,
+  aPnThirdPartyMessage,
   aPNServiceId,
+  aPNThirdPartyNotification,
   base64File
 } from "../../__mocks__/pn";
 
@@ -72,7 +72,7 @@ mockNodeFetch.mockImplementation(
       ok: true,
       status: 200,
       json: async () => {
-        return aPnNotification;
+        return aPnThirdPartyMessage;
       }
     } as Response)
 );
@@ -152,16 +152,7 @@ describe("third-party-service-client", () => {
     expect(res).toMatchObject(
       E.right({
         status: 200,
-        value: {
-          details: aPnNotificationObject,
-          attachments: [
-            {
-              content_type: aPnNotification.documents[0].contentType,
-              id: `${aThirdPartyId}${aPnNotification.documents[0].docIdx}`,
-              url: `/delivery/notifications/sent/${aThirdPartyId}/attachments/documents/${aPnNotification.documents[0].docIdx}`
-            }
-          ]
-        }
+        value: aPNThirdPartyNotification
       })
     );
 
@@ -169,6 +160,7 @@ describe("third-party-service-client", () => {
       `${expectedConfig.baseUrl}/delivery/notifications/received/${aThirdPartyId}`,
       {
         headers: {
+          Accept: "application/io+json",
           "x-pagopa-cx-taxid": aProdFiscalCode,
           "x-api-key": "aKey",
           [expectedConfig.detailsAuthentication.header_key_name]:
