3cd5babdf57659e7be59cbc81ccbdbe85a1a6ccf
arcogabbo <arcogabbo@gmail.com>
2023-07-03 12:44:54 +0200

    [#IOPID-269] added controller errors unit tests

--- src/controllers/__tests__/fastLoginController.test.ts
+++ src/controllers/__tests__/fastLoginController.test.ts
@@ -2,6 +2,7 @@ import RedisSessionStorage from "../../services/redisSessionStorage";
 import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
 import * as TE from "fp-ts/lib/TaskEither";
+import * as RNEA from "fp-ts/lib/ReadonlyNonEmptyArray";
 import * as redis from "redis";
 import { ResponseSuccessJson } from "@pagopa/ts-commons/lib/responses";
 import { aFiscalCode } from "../../__mocks__/user_mock";
@@ -17,6 +18,7 @@ import { getFastLoginLollipopConsumerClient } from "../../clients/fastLoginLolli
 import {
   aLollipopOriginalUrl,
   anAssertionRef,
+  anotherAssertionRef,
   aSignature,
   aSignatureInput,
 } from "../../__mocks__/lollipop";
@@ -29,6 +31,9 @@ import { LollipopPublicKey } from "../../../generated/fast-login-api/LollipopPub
 import { SessionToken } from "../../types/token";
 import { aSAMLResponse } from "../../utils/__mocks__/spid";
 import { FastLoginResponse as LCFastLoginResponse } from "../../../generated/fast-login-api/FastLoginResponse";
+import { BadRequest } from "../../../generated/fast-login-api/BadRequest";
+import * as spidUtils from "../../utils/spid";
+import { pipe } from "fp-ts/lib/function";
 
 const mockSet = jest.fn();
 const mockGetBySessionToken = jest.fn();
@@ -176,6 +181,10 @@ const controller = fastLoginEndpoint(
 );
 
 describe("fastLoginController", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should create a valid session of 15 minutes given a valid payload", async () => {
     const validUserSetPayload = {
       session_token: aRandomToken,
@@ -216,7 +225,311 @@ describe("fastLoginController", () => {
     expect(mockSet).toHaveBeenCalledTimes(1);
     expect(mockSet).toHaveBeenCalledWith(validUserSetPayload, sessionTTL);
 
+    expect(mockGetNewToken).toHaveBeenCalledTimes(7);
+
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith(validFastLoginControllerResponse);
+  });
+
+  it("should fail when lollipop locals are invalid", async () => {
+    const response = await controller(mockReq(), {
+      ...fastLoginLollipopLocals,
+      "x-pagopa-lollipop-user-id": "NOTAFISCALCODE",
+    });
+    const res = mockRes();
+    response.apply(res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Could not initialize Lollipop",
+      })
+    );
+  });
+
+  it("should fail when the sessionStorage had a connection error", async () => {
+    mockGetLollipop.mockRejectedValueOnce(null);
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Error while trying to get Lollipop initialization",
+      })
+    );
+  });
+
+  it("should fail when the sessionStorage can't find an assertionRef related to the user", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.left(new Error("error")));
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Error while validating Lollipop initialization: error",
+      })
+    );
+  });
+
+  it.each`
+    scenario                                                            | storageResult
+    ${"the assertionRef was not found"}                                 | ${O.none}
+    ${"the assertionRef is not the same as the one in lollipop locals"} | ${O.some(anotherAssertionRef)}
+  `("should return 403 when $title", async ({ storageResult }) => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(storageResult));
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(403);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "You are not allowed here",
+        detail:
+          "You do not have enough permission to complete the operation you requested",
+      })
+    );
+  });
+
+  it("should fail when the lollipop consumer can't be contacted", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockRejectedValueOnce(null);
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Error while calling the Lollipop Consumer",
+      })
+    );
+  });
+
+  it("should fail when the lollipop consumer gives a decoding error", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(BadRequest.decode({}));
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: expect.stringContaining(
+          "Unexpected Lollipop consumer response"
+        ),
+      })
+    );
+  });
+
+  it("should return 401 when the lollipop consumer gives a 401 Unauthorized", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(E.right({ status: 401 }));
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Unauthorized",
+        detail: "Invalid signature or nonce expired",
+      })
+    );
+  });
+
+  it.each`
+    title                   | status
+    ${"500"}                | ${500}
+    ${"other status codes"} | ${599}
+  `(
+    "should return 500 when the lollipop consumer gives $title",
+    async ({ status }) => {
+      mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+      mockLCFastLogin.mockResolvedValueOnce(
+        E.right({ status: status, value: { detail: "error", title: "error" } })
+      );
+      const response = await controller(mockReq(), fastLoginLollipopLocals);
+      const res = mockRes();
+      response.apply(res);
+
+      expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+      expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith(
+        expect.objectContaining({
+          title: "Internal server error",
+          detail: `Error in Lollipop consumer. Response contains ${status} with title error and detail error`,
+        })
+      );
+    }
+  );
+
+  it("should return 500 when the lollipop consumer gives an invalid saml_response", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(
+      E.right({ status: 200, value: { saml_response: "" } })
+    );
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Could not parse saml response from Lollipop consumer",
+      })
+    );
+  });
+
+  it("should return 403 when the user is blocked", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(
+      E.right({ status: 200, value: validFastLoginLCResponse })
+    );
+    mockIsBlockedUser.mockResolvedValueOnce(E.right(true));
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(mockIsBlockedUser).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Unauthorized",
+        detail: "User is blocked",
+      })
+    );
+  });
+
+  it("should return 500 when the session storage could not determine if the user is blocked", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(
+      E.right({ status: 200, value: validFastLoginLCResponse })
+    );
+    mockIsBlockedUser.mockResolvedValueOnce(E.left(new Error("error")));
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(mockIsBlockedUser).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Error while validating user",
+      })
+    );
+  });
+
+  it("should return 500 when the controller can't extract the data from the saml_response", async () => {
+    const makeProxyUser = jest.spyOn(
+      spidUtils,
+      "makeProxyUserFromSAMLResponse"
+    );
+
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(
+      E.right({ status: 200, value: validFastLoginLCResponse })
+    );
+    mockIsBlockedUser.mockResolvedValueOnce(E.right(false));
+    makeProxyUser.mockReturnValueOnce(O.none);
+
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(mockIsBlockedUser).toHaveBeenCalledTimes(1);
+    expect(makeProxyUser).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Could not create proxy user",
+      })
+    );
+  });
+
+  it("should return 500 when the session storage could not create the session for the user", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(
+      E.right({ status: 200, value: validFastLoginLCResponse })
+    );
+    mockIsBlockedUser.mockResolvedValueOnce(E.right(false));
+    mockSet.mockReturnValueOnce(E.left(new Error("error")));
+
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(mockIsBlockedUser).toHaveBeenCalledTimes(1);
+    expect(mockSet).toHaveBeenCalledTimes(1);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: "Could not create user using session storage: error",
+      })
+    );
+  });
+
+  it("should return 500 when the session token created is of the wrong type", async () => {
+    mockGetLollipop.mockResolvedValueOnce(E.right(O.some(anAssertionRef)));
+    mockLCFastLogin.mockResolvedValueOnce(
+      E.right({ status: 200, value: validFastLoginLCResponse })
+    );
+    mockIsBlockedUser.mockResolvedValueOnce(E.right(false));
+    mockSet.mockReturnValueOnce(E.right(true));
+    pipe(
+      RNEA.range(0, 6),
+      RNEA.map(() => mockGetNewToken.mockResolvedValueOnce(""))
+    );
+
+    const response = await controller(mockReq(), fastLoginLollipopLocals);
+    const res = mockRes();
+    response.apply(res);
+
+    expect(mockGetLollipop).toHaveBeenCalledTimes(1);
+    expect(mockLCFastLogin).toHaveBeenCalledTimes(1);
+    expect(mockIsBlockedUser).toHaveBeenCalledTimes(1);
+    expect(mockSet).toHaveBeenCalledTimes(1);
+    expect(mockGetNewToken).toHaveBeenCalledTimes(7);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        title: "Internal server error",
+        detail: expect.stringContaining("Could not decode session token"),
+      })
+    );
   });
 });
