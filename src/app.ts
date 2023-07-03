01580ad46d31089903d6fbb62c09a9013cbec2bd
arcogabbo <arcogabbo@gmail.com>
2023-07-03 08:58:32 +0200

    [#IOPID-269] WIP: fix linter error

--- src/app.ts
+++ src/app.ts
@@ -536,7 +536,7 @@ export async function newApp({
           authMiddlewares.local
         );
 
-        if (FF_FAST_LOGIN != FeatureFlagEnum.NONE) {
+        if (FF_FAST_LOGIN !== FeatureFlagEnum.NONE) {
           // eslint-disable-next-line @typescript-eslint/no-use-before-define
           registerFastLoginRoutes(
             app,
