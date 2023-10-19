3e0cb21fe6fe65b596885819a7f69957e5f9e71d
Michael Disaro <michael.disaro@gmail.com>
2023-10-19 14:37:04 +0200

    modified default value for retry after of attachments

--- src/services/newMessagesService.ts
+++ src/services/newMessagesService.ts
@@ -621,7 +621,7 @@ export default class NewMessagesService {
                 case 500:
                   return ResponseErrorInternal(ERROR_MESSAGE_500);
                 case 503:
-                  const retryAfter = response.headers["Retry-After"] ?? "5";
+                  const retryAfter = response.headers["Retry-After"] ?? "10";
                   return ResponseErrorServiceTemporarilyUnavailable(ERROR_MESSAGE_503, retryAfter);
                 default:
                   return ResponseErrorStatusNotDefinedInSpec(response);
