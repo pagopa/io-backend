2d91860ea9aa6f30aa5eea3569431f263f06d833
Andrea Grillo <andrea.grillo@pagopa.it>
2023-12-18 13:07:11 +0100

    upgrade azurerm

--- .identity/00_main.tf
+++ .identity/00_main.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.33.0"
+      version = "<=3.84.0"
     }
     github = {
       source  = "integrations/github"
