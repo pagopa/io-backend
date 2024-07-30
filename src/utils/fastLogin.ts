b60ab4f47b4ce48c9a5eafe0d0972885e2676dca
Daniele Manni <dan.manni@gmail.com>
2024-07-30 10:52:18 +0200

    [#IOPID-1771] remove acsRequestMapper, getLoginType, spidLogCallback

--- src/utils/fastLogin.ts
+++ src/utils/fastLogin.ts
@@ -1,5 +1,4 @@
 import * as t from "io-ts";
-import * as express from "express";
 import { enumType } from "@pagopa/ts-commons/lib/types";
 
 export enum LoginTypeEnum {
@@ -9,34 +8,8 @@ export enum LoginTypeEnum {
 export type LoginTypeT = t.TypeOf<typeof LoginType>;
 export const LoginType = enumType<LoginTypeEnum>(LoginTypeEnum, "LoginType");
 
-export type AdditionalLoginPropsT = t.TypeOf<typeof AdditionalLoginProps>;
-export const AdditionalLoginProps = t.partial({ loginType: LoginType });
-
 export const ActiveSessionInfo = t.type({
   ttl: t.number,
   type: LoginType,
 });
-
 export type ActiveSessionInfo = t.TypeOf<typeof ActiveSessionInfo>;
-
-export const acsRequestMapper = (
-  req: express.Request
-): t.Validation<AdditionalLoginPropsT> =>
-  AdditionalLoginProps.decode({
-    loginType: req.header("x-pagopa-login-type"),
-  });
-
-// ----------------------------
-// FF management
-// ----------------------------
-
-export const getLoginType = (
-  loginType: LoginTypeEnum | undefined,
-  isUserEligibleForFastLogin: boolean,
-  isLollipopEnabled: boolean
-): LoginTypeEnum =>
-  isLollipopEnabled &&
-  loginType === LoginTypeEnum.LV &&
-  isUserEligibleForFastLogin
-    ? LoginTypeEnum.LV
-    : LoginTypeEnum.LEGACY;
