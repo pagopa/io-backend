bb0d1f70f3e320069cbfde2a53611ab4d7df1d53
silvicir <127971996+silvicir@users.noreply.github.com>
2024-11-19 17:13:38 +0100

    chore: added trial system FF
    Refs: #1172 SIW-1849

--- src/controllers/trialController.ts
+++ src/controllers/trialController.ts
@@ -12,15 +12,21 @@ import {
   IResponseSuccessAccepted,
   IResponseSuccessJson,
   IResponseSuccessRedirectToResource,
+  ResponseSuccessJson,
 } from "@pagopa/ts-commons/lib/responses";
 
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import TrialService from "src/services/trialService";
+import {
+  FF_IO_WALLET_TRIAL_ENABLED,
+  IO_WALLET_TRIAL_ID,
+} from "../../src/config";
 import { TrialId } from "../../generated/trial-system-api/TrialId";
 import { withUserFromRequest } from "../types/user";
 
 import { withValidatedOrValidationError } from "../utils/responses";
 import { Subscription } from "../../generated/trial-system/Subscription";
+import { SubscriptionStateEnum } from "../../generated/trial-system/SubscriptionState";
 
 export default class TrialController {
   // eslint-disable-next-line max-params
@@ -59,10 +65,19 @@ export default class TrialController {
       withValidatedOrValidationError(
         TrialId.decode(req.params.trialId),
         (trialId) =>
-          withValidatedOrValidationError(
-            NonEmptyString.decode(user.fiscal_code),
-            (userId) => this.trialService.getSubscription(userId, trialId)
-          )
+          // if the trialId is not the one from the wallet, the trial system is always called; otherwise, we check the value of FF_IO_WALLET_TRIAL_ENABLED
+          trialId !== IO_WALLET_TRIAL_ID || FF_IO_WALLET_TRIAL_ENABLED
+            ? withValidatedOrValidationError(
+                NonEmptyString.decode(user.fiscal_code),
+                (userId) => this.trialService.getSubscription(userId, trialId)
+              )
+            : Promise.resolve(
+                ResponseSuccessJson({
+                  createdAt: new Date(),
+                  state: SubscriptionStateEnum.ACTIVE,
+                  trialId,
+                })
+              )
       )
     );
 }
