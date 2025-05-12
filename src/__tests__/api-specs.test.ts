ea316ea1241c13184ad6e6f2e51836c65b32ee1a
Daniele Manni <dan.manni@gmail.com>
2025-05-12 10:37:30 +0200

    Add missing openapi validation unit test

--- src/__tests__/api-specs.test.ts
+++ src/__tests__/api-specs.test.ts
@@ -1,27 +1,66 @@
 import * as SwaggerParser from "swagger-parser";
 
-describe("API proxy specs", () => {
-  const specFilePath = `${__dirname}/../../openapi/generated/api_auth.yaml`;
-
-  it("should be valid", async () => {
+describe("API public proxy specs", () => {
+  it("should be valid specs for auth domain", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_auth.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API cgn domain specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_cgn.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API communication domain specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_communication.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API FIMS specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_io_fims.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API io-sign specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_io_sign.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API io-wallet domain specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_io_wallet.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API payments domain specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_payments.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API platform domain specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_platform.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API services domain specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_services.yaml`;
+    const api = await SwaggerParser.bundle(specFilePath);
+    expect(api).toBeDefined();
+  });
+  it("should be valid API EUCovidCert specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_eucovidcert.yaml`;
     const api = await SwaggerParser.bundle(specFilePath);
     expect(api).toBeDefined();
   });
 });
 
-describe("API notifications specs", () => {
-  const specFilePath = `${__dirname}/../../openapi/generated/api_notifications.yaml`;
-
-  it("should be valid", async () => {
+describe("API internal ", () => {
+  it("should be valid auth domain for sessions specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_session.yaml`;
     const api = await SwaggerParser.bundle(specFilePath);
     expect(api).toBeDefined();
   });
-});
-
-describe("API EUCovidCert specs", () => {
-  const specFilePath = `${__dirname}/../../openapi/generated/api_eucovidcert.yaml`;
-
-  it("should be valid", async () => {
+  it("should be valid API notifications specs", async () => {
+    const specFilePath = `${__dirname}/../../openapi/generated/api_notifications.yaml`;
     const api = await SwaggerParser.bundle(specFilePath);
     expect(api).toBeDefined();
   });
