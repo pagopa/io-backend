c581aa01fc55f1542c2197ab8cf6076bb37eeadd
Luca Mancosu <luca.mancosu@it.ey.com>
2025-05-16 15:29:46 +0200

    add logic for gradually migrate the create/update/delete installation and notify messages

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -29,11 +29,25 @@ import {
 } from "../../generated/messages/NotifyMessage";
 import { Notification } from "../../generated/notifications/Notification";
 import { SuccessResponse } from "../../generated/notifications/SuccessResponse";
+import {
+  IO_COM_PUSH_NOTIFICATIONS_QUEUE_NAME,
+  IO_COM_PUSH_NOTIFICATIONS_REDIRECT_PERCENTAGE,
+  IO_COM_QUEUE_STORAGE_CONNECTION_STRING
+} from "../config";
 import { toFiscalCodeHash } from "../types/notification";
 import { base64EncodeObject } from "../utils/messages";
 
+const redirectOnNewPushNotifyQueue = (): boolean => {
+  const redirectionPercentage = parseFloat(
+    IO_COM_PUSH_NOTIFICATIONS_REDIRECT_PERCENTAGE
+  );
+  return Math.random() < redirectionPercentage;
+};
+
 export default class NotificationService {
   private readonly notificationQueueClient: QueueClient;
+  private readonly newNotificationQueueClient: QueueClient;
+
   constructor(
     private readonly queueStorageConnectionString: string,
     private readonly queueName: string
@@ -41,6 +55,17 @@ export default class NotificationService {
     this.notificationQueueClient = new QueueClient(
       this.queueStorageConnectionString,
       this.queueName
+    );
+
+    /**
+     * A new queue client pointing to the new push-notification queue
+     * This queue client will be removed, or will replace the notificationQueueClient,
+     * when all the create/update installation and notifications traffic has been totally redirected
+     * to the new push-notification queue
+     */
+    this.newNotificationQueueClient = new QueueClient(
+      IO_COM_QUEUE_STORAGE_CONNECTION_STRING,
+      IO_COM_PUSH_NOTIFICATIONS_QUEUE_NAME
     );
   }
 
@@ -64,7 +89,12 @@ export default class NotificationService {
         )
       }
     };
-    return this.notificationQueueClient
+
+    const queueClient = redirectOnNewPushNotifyQueue()
+      ? this.newNotificationQueueClient
+      : this.notificationQueueClient;
+
+    return queueClient
       .sendMessage(base64EncodeObject(notifyMessage))
       .then(() => ResponseSuccessJson({ message: "ok" }))
       .catch((error) =>
@@ -92,7 +122,13 @@ export default class NotificationService {
       pushChannel: installation.pushChannel,
       tags: [toFiscalCodeHash(fiscalCode)]
     };
-    return this.notificationQueueClient
+    //generazione del numero casuale oppure test dell'espressione regolare sul fiscalCode
+
+    const queueClient = redirectOnNewPushNotifyQueue()
+      ? this.newNotificationQueueClient
+      : this.notificationQueueClient;
+
+    return queueClient
       .sendMessage(base64EncodeObject(azureInstallation))
       .then(() => ResponseSuccessJson({ message: "ok" }))
       .catch((error) =>
@@ -111,7 +147,12 @@ export default class NotificationService {
       installationId: toFiscalCodeHash(fiscalCode),
       kind: DeleteKind[NotificationMessageKindEnum.DeleteInstallation]
     };
-    return this.notificationQueueClient
+
+    const queueClient = redirectOnNewPushNotifyQueue()
+      ? this.newNotificationQueueClient
+      : this.notificationQueueClient;
+
+    return queueClient
       .sendMessage(base64EncodeObject(deleteMessage))
       .then(() => ResponseSuccessJson({ message: "ok" }))
       .catch((error) =>
