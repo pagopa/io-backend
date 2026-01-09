74a4bdab17e5d7a98158eb9329a1505509bc1e49
arcogabbo <arcogabbo@gmail.com>
2026-01-09 15:43:21 +0100

    [#IOPLT-1472] CDC: mounted new routes

--- src/routes/cdcSupportRoutes.ts
+++ src/routes/cdcSupportRoutes.ts
@@ -1,19 +1,22 @@
 import { Express } from "express";
+import * as express from "express";
 import * as passport from "passport";
 
 import CdcSupportController from "../controllers/cdcSupportController";
 import CdcSupportService from "../services/cdcSupportService";
 import { toExpressHandler } from "../utils/express";
 
+export const CDC_SUPPORT_PLATFORM_API_BASE_PATH = "/api/cdc-support/v1";
+
 /**
- * Mount the cdc routes into the Express application
+ * Mount the Legacy cdc routes into the Express application
  *
  * @param app The Express application
  * @param basePath The base path for the cdc support APIs
  * @param cdcSupportService The service that handles the cdc support requests
  * @param bearerSessionTokenAuth The autentication middleware for user session token
  */
-export const registerCdcSupportAPIRoutes = (
+export const registerLegacyCdcSupportAPIRoutes = (
   app: Express,
   basePath: string,
   cdcSupportService: CdcSupportService,
@@ -26,6 +29,30 @@ export const registerCdcSupportAPIRoutes = (
   app.get(
     `${basePath}/status`,
     bearerSessionTokenAuth,
+    toExpressHandler(cdcSupportController.status, cdcSupportController)
+  );
+};
+
+/**
+ * Mount the cdc routes into the Express application
+ *
+ * @param app The Express application
+ * @param cdcSupportService The service that handles the cdc support requests
+ * @param authMiddleware The autentication middleware for user session token
+ */
+export const registerCdcSupportAPIRoutes = (
+  app: Express,
+  cdcSupportService: CdcSupportService,
+  authMiddleware: express.RequestHandler
+): void => {
+  const basePath = CDC_SUPPORT_PLATFORM_API_BASE_PATH;
+  const cdcSupportController: CdcSupportController = new CdcSupportController(
+    cdcSupportService
+  );
+
+  app.get(
+    `${basePath}/status`,
+    authMiddleware,
     toExpressHandler(cdcSupportController.status, cdcSupportController)
   );
 };
