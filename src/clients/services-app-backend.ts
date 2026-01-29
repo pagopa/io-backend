d2da1ab8397595464c92455362c51fcb495a296c
salvatorelaiso <salvatorelaiso@gmail.com>
2026-01-29 09:27:27 +0100

    fix: correct token property name to `ApiKeyAuth` in `ServicesAppBackendAPIClient`

--- src/clients/services-app-backend.ts
+++ src/clients/services-app-backend.ts
@@ -19,7 +19,7 @@ export function ServicesAppBackendAPIClient(
     withDefaults: (op) => (params) =>
       op({
         ...params,
-        ApiKey: token
+        ApiKeyAuth: token
       })
   });
 }
