e22e0ae69c61faf5c06b9793239c6576265ee0ba
Daniele Manni <dan.manni@gmail.com>
2026-06-15 10:53:43 +0200

    Remove useless linter rule exclusion

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -2,7 +2,6 @@ import { User } from "../../types/user";
 
 declare global {
   namespace Express {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface Request {
       user?: User;
     }
